67855ab30f9f40e98bd58ed306616a074d2b1ab5
Michaël De Boey <info@michaeldeboey.be>
2019-05-06 13:33:30 +0200

    Add TypeScript typings

---
+++ typings/index.d.ts
@@ -0,0 +1,48 @@
+type Page = {
+  component: string,
+  props: object,
+  url: string,
+  version: string | null,
+}
+
+type SpecificVisitOptions = {
+  preserveScroll?: boolean,
+  replace?: boolean,
+}
+type SpecificVisit = (
+  url: string,
+  data?: object,
+  options?: SpecificVisitOptions
+) => Promise<void>
+
+type ReloadOptions = ReplaceOptions
+
+type RememberData = object
+
+type ReplaceOptions = {
+  data?: object,
+  method?: string,
+  preserveScroll?: boolean,
+}
+
+type VisitOptions = ReplaceOptions & SpecificVisitOptions
+
+declare const Inertia: {
+  delete: SpecificVisit,
+  init: (page: Page, setPage: (page: Page) => void) => void,
+  patch: SpecificVisit,
+  post: SpecificVisit,
+  put: SpecificVisit,
+  reload: (options?: ReloadOptions) => Promise<void>,
+  remember: (data: RememberData, key?: string) => void,
+  replace: (url: string, options?: ReplaceOptions) => Promise<void>,
+  restore: (key?: string) => RememberData,
+  visit: (url: string, options?: VisitOptions) => Promise<void>,
+}
+
+declare function shouldIntercept(
+  event: HTMLInputElement | KeyboardEvent
+): boolean
+
+export default Inertia
+export { Page, shouldIntercept }
