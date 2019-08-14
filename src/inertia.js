inertiajs/inertia

commit b50230280c419aaa4b00d3b05638a596131a2542

Add bundler

src/inertia.js
@@ -159,10 +159,9 @@ export default {
   },
 
   remember(data, key = 'default') {
-    this.setState({
-      ...window.history.state,
-      cache: { ...window.history.state.cache, [key]: data },
-    })
+    let newState = { ...window.history.state }
+    newState.cache[key] = data
+    this.setState(newState)
   },
 
   restore(key = 'default') {

.gitignore
@@ -1,3 +1,4 @@
 /node_modules
 .DS_Store
 package-lock.json
+dist

commit 5c244b4ad0514162ba7877566223017c917823b2

Fix remember regression introduced in b502302

src/inertia.js
@@ -175,6 +175,7 @@ export default {
 
   remember(data, key = 'default') {
     let newState = { ...window.history.state }
+    newState.cache = newState.cache || {}
     newState.cache[key] = data
     this.setState(newState)
   },