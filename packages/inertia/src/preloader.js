45fc777287d06987f66963b6453df28cabad693a
Claudio Dekker <claudiodekker@users.noreply.github.com>
2020-09-18 11:11:19 +0200

    Preloader: Support cancelTokens

--- packages/inertia/src/preloader.js
+++ packages/inertia/src/preloader.js
@@ -1,23 +1,34 @@
+import Axios from 'axios'
+
 export default {
   requests: {},
   flush() {
+    Object.keys(this.requests).forEach(url => this.requests[url].cancelToken.cancel())
     this.requests = {}
   },
   load(url, handler) {
     if (Object.keys(this.requests).indexOf(url) === -1) {
+      const cancelToken = Axios.CancelToken.source()
       this.requests[url] = {
         callbacks: [],
         response: null,
+        cancelToken,
       }
 
-      return Promise.resolve(handler()).then(response => {
-        this.requests[url].response = response
+      return Promise.resolve(handler(cancelToken))
+        .then(response => {
+          this.requests[url].response = response
 
-        this.requests[url].callbacks.forEach(callback => callback(response))
-        this.requests[url].callbacks = []
+          this.requests[url].callbacks.forEach(callback => callback(response))
+          this.requests[url].callbacks = []
 
-        return response
-      })
+          return response
+        })
+        .catch(error => {
+          if (! Axios.isCancel(error)) {
+            Promise.reject(error)
+          }
+        })
     }
 
     if (this.requests[url].response) {
