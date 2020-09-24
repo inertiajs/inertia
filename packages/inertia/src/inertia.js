9105983151feba6ea5c02a7299386145e789d280
Claudio Dekker <claudiodekker@users.noreply.github.com>
2020-09-24 10:24:53 +0200

    Properly scope changes

--- packages/inertia/src/inertia.js
+++ packages/inertia/src/inertia.js
@@ -204,10 +204,10 @@ export default {
     return Promise.resolve(this.resolveComponent(page.component)).then(component => {
       if (visitId === this.visitId) {
         preserveState = typeof preserveState === 'function' ? preserveState(page.props) : preserveState
-        preserveScroll = typeof preserveScroll === 'function' ? preserveScroll(page.props) : preserveScroll
         replace = replace || page.url === `${window.location.pathname}${window.location.search}`
         replace ? this.replaceState(page, preserveState) : this.pushState(page)
         this.updatePage(component, page.props, { preserveState }).then(() => {
+          preserveScroll = typeof preserveScroll === 'function' ? preserveScroll(page.props) : preserveScroll
           if (!preserveScroll) {
             this.resetScrollPositions()
           }
