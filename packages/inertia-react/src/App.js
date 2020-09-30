f6304c6e179ad5cf666df4d07546cce3c38d7e64
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2020-09-30 15:39:35 +0200

    React shorthand nested layouts

--- packages/inertia-react/src/App.js
+++ packages/inertia-react/src/App.js
@@ -37,8 +37,15 @@ export default function App({
   const renderChildren = children || (({ Component, props, key }) => {
     const child = createElement(Component, { key, ...props })
 
-    if (Component.layout) {
+    if (typeof Component.layout === 'function') {
       return Component.layout(child)
+    }
+
+    if (Array.isArray(Component.layout)) {
+      return Component.layout
+        .concat(child)
+        .reverse()
+        .reduce((children, Layout) => createElement(Layout, { children }))
     }
 
     return child
