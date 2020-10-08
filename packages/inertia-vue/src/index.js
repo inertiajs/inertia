992cc684800de341270e11b3ed3503d930196209
Jonathan Reinink <jonathan@reinink.ca>
2020-10-08 09:55:50 -0400

    Add non-namespaced exports to all adapters

--- packages/inertia-vue/src/index.js
+++ packages/inertia-vue/src/index.js
@@ -1,3 +1,3 @@
-export { default as InertiaApp } from './app'
-export { default as InertiaLink } from './link'
-export { default as InertiaPlugin } from './plugin'
+export { default as app, default as InertiaApp } from './app'
+export { default as link, default as InertiaLink } from './link'
+export { default as plugin } from './plugin'
