5251a4497a679df254fac35dd337fb3ca23e612a
Jonathan Reinink <jonathan@reinink.ca>
2020-10-20 06:52:37 -0400

    Update shouldIntercept to handle non-anchor elements

--- packages/inertia/src/shouldIntercept.js
+++ packages/inertia/src/shouldIntercept.js
@@ -1,11 +1,12 @@
 export default function shouldIntercept(event) {
+  const isLink = event.currentTarget.tagName.toLowerCase() === 'a'
   return !(
     (event.target && event.target.isContentEditable)
     || event.defaultPrevented
-    || event.which > 1
-    || event.altKey
-    || event.ctrlKey
-    || event.metaKey
-    || event.shiftKey
+    || (isLink && event.which > 1)
+    || (isLink && event.altKey)
+    || (isLink && event.ctrlKey)
+    || (isLink && event.metaKey)
+    || (isLink && event.shiftKey)
   )
 }
