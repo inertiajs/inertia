12d958d12ccf51a3136c1294345cfcf957f7b88f
Matthew Hailwood <matthew@hailwood.nz>
2020-12-16 00:06:37 +1300

    Add a displayName to the PageContext (#351)

--- packages/inertia-react/src/PageContext.js
+++ packages/inertia-react/src/PageContext.js
@@ -1,3 +1,6 @@
 import { createContext } from 'react'
 
-export default createContext()
+const pageContext = createContext()
+pageContext.displayName = 'InertiaPageContext'
+
+export default pageContext
