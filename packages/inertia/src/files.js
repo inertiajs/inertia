5182f2f8bb2621b0f29120a1a66d7b330e8b306f
Jonathan Reinink <jonathan@reinink.ca>
2020-12-22 16:36:45 -0500

    Simplify "hasFiles" method

--- packages/inertia/src/files.js
+++ packages/inertia/src/files.js
@@ -1,41 +1,5 @@
-export function isFile(object) {
-  return object instanceof File || object instanceof FileList
-}
-
-export function hasFilesDeep(object) {
-  if (object === null) {
-    return false
-  }
-
-  if (typeof object === 'object') {
-    for (const key in object) {
-      if (Object.prototype.hasOwnProperty.call(object, key)) {
-        if (hasFilesDeep(object[key])) {
-          return true
-        }
-      }
-    }
-  }
-
-  if (Array.isArray(object)) {
-    for (const key in object) {
-      if (Object.prototype.hasOwnProperty.call(object, key)) {
-        return hasFilesDeep(object[key])
-      }
-    }
-  }
-
-  return isFile(object)
-}
-
-export function hasFiles(form) {
-  for (const property in form) {
-    if (Object.prototype.hasOwnProperty.call(form, property)) {
-      if (hasFilesDeep(form[property])) {
-        return true
-      }
-    }
-  }
-
-  return false
+export function hasFiles(data) {
+  return data instanceof File
+    || data instanceof FileList
+    || (typeof data === 'object' && data !== null && Object.values(data).find(value => hasFiles(value)) !== undefined)
 }
