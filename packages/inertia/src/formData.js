e81e057fec81782a8c7b1ba7a0a2e21bb60652a2
Jonathan Reinink <jonathan@reinink.ca>
2020-12-23 06:44:21 -0500

    Tweak formatting of appendToFormData function

--- packages/inertia/src/formData.js
+++ packages/inertia/src/formData.js
@@ -19,23 +19,15 @@ function getKey(parent, property) {
 function appendToFormData(formData, key, value) {
   if (value instanceof Date) {
     return formData.append(key, value.toISOString())
-  }
-
-  if (value instanceof File) {
+  } else if (value instanceof File) {
     return formData.append(key, value, value.name)
-  }
-
-  if (typeof value === 'boolean') {
+  } else if (typeof value === 'boolean') {
     return formData.append(key, value ? '1' : '0')
-  }
-
-  if (value === null) {
+  } else if (value === null) {
     return formData.append(key, '')
-  }
-
-  if (typeof value !== 'object') {
+  } else if (typeof value !== 'object') {
     return formData.append(key, value)
+  } else {
+    objectToFormData(value, formData, key)
   }
-
-  objectToFormData(value, formData, key)
 }
