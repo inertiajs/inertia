ec171af087556a1ae9537026f0cff6d4f680f5af
Claudio Dekker <1752195+claudiodekker@users.noreply.github.com>
2021-01-04 17:41:48 +0100

    WIP

---
+++ packages/inertia-vue/tests/cypress/integration/plugin.test.js
@@ -0,0 +1,7 @@
+describe('Plugin', () => {
+  it('registers the Inertia plugin using the deprecated "app" component', () => {
+    cy.visit('/use-deprecated-vue-plugin')
+
+      
+  })
+})
--- packages/inertia-vue/tests/src/app.js
+++ packages/inertia-vue/tests/src/app.js
@@ -1,17 +1,21 @@
-import Vue from "vue"
-import { InertiaApp } from "@inertiajs/inertia-vue"
+import Vue from 'vue'
+import { InertiaApp, plugin } from '@inertiajs/inertia-vue'
 
-Vue.use(InertiaApp)
+if (window.location.href.indexOf('use-deprecated-vue-plugin') > -1) {
+  Vue.use(InertiaApp)
+} else {
+  Vue.use(plugin)
+}
 
 const app = document.getElementById('app')
 
 window.vm = new Vue({
-    render: h => h(InertiaApp, {
-        props: {
-            initialPage: JSON.parse(app.dataset.page),
-            resolveComponent: name => {
-                return import(`./Pages/${name}`).then(module => module.default)
-            },
-        },
-    }),
+  render: h => h(InertiaApp, {
+    props: {
+      initialPage: JSON.parse(app.dataset.page),
+      resolveComponent: name => {
+        return import(`./Pages/${name}`).then(module => module.default)
+      },
+    },
+  }),
 }).$mount(app)
