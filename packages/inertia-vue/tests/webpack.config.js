cfeeb2bffcdc1fb842080bd71562af286913ee96
Claudio Dekker <1752195+claudiodekker@users.noreply.github.com>
2021-01-04 15:42:40 +0100

    WIP

--- packages/inertia-vue/tests/webpack.config.js
+++ packages/inertia-vue/tests/webpack.config.js
@@ -2,29 +2,25 @@ const path = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
-    entry: './src/app.js',
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader'
-            }
-        ]
-    },
-    output: {
-        filename: 'app.js',
-        path: path.resolve(__dirname, 'tmp'),
-    },
-    plugins: [
-        new VueLoaderPlugin()
+  entry: './src/app.js',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
     ],
-    resolve: {
-        extensions: ['*', '.js', '.vue'],
-        alias: {
-            '@inertiajs/inertia': path.resolve(__dirname, '../../packages/inertia/dist/index.js'),
-            '@inertiajs/inertia-vue': path.resolve(__dirname, '../../packages/inertia-vue/dist/index.js')
-        }
-    },
-    stats: 'errors-only',
+  },
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'tmp'),
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+  ],
+  resolve: {
+    extensions: ['*', '.js', '.vue'],
+  },
+  stats: 'errors-only',
 }
