3d285abe4b8fec63daa3c23c89230ea9568a2420
Claudio Dekker <1752195+claudiodekker@users.noreply.github.com>
2021-01-04 17:22:36 +0100

    Add ESLint linting on tests

--- packages/inertia-vue/tests/cypress/integration/inertia.test.js
+++ packages/inertia-vue/tests/cypress/integration/inertia.test.js
@@ -1,34 +1,34 @@
 describe('Inertia', () => {
-    it('mounts the initial page', () => {
-        cy.visit('/')
+  it('mounts the initial page', () => {
+    cy.visit('/')
 
-        cy.get('body > div:first-child').should(($el) => {
-            expect($el.get(0).innerHTML.trim()).to.eq('Hello World!')
-        })
+    cy.get('body > div:first-child').should(($el) => {
+      expect($el.get(0).innerHTML.trim()).to.eq('Hello World!')
     })
+  })
 
-    it('has the $inertia app injected into page component', () => {
-        cy.visit('/')
+  it('has the $inertia app injected into page component', () => {
+    cy.visit('/')
 
-        cy.window().then(win => {
-            const inertiaRoot = win.vm.$children[0]
-            const page = inertiaRoot.$children[0]
+    cy.window().then(win => {
+      const inertiaRoot = win.vm.$children[0]
+      const page = inertiaRoot.$children[0]
 
-            expect(page.$inertia).to.be.an('object')
-            expect(page.$inertia).to.have.property('init')
-            expect(page.$inertia).to.have.property('resolveComponent')
-            expect(page.$inertia).to.have.property('page')
-        })
+      expect(page.$inertia).to.be.an('object')
+      expect(page.$inertia).to.have.property('init')
+      expect(page.$inertia).to.have.property('resolveComponent')
+      expect(page.$inertia).to.have.property('page')
     })
+  })
 
-    it('has the $page helper injected into the Vue component', () => {
-        cy.visit('/')
+  it('has the $page helper injected into the Vue component', () => {
+    cy.visit('/')
 
-        cy.window().then(win => {
-            const inertiaRoot = win.vm.$children[0]
-            const page = inertiaRoot.$children[0]
+    cy.window().then(win => {
+      const inertiaRoot = win.vm.$children[0]
+      const page = inertiaRoot.$children[0]
 
-            expect(page.$page).to.deep.equal(page.$inertia.page.props)
-        })
+      expect(page.$page).to.deep.equal(page.$inertia.page.props)
     })
+  })
 })
--- packages/inertia-vue/tests/cypress/integration/links.test.js
+++ packages/inertia-vue/tests/cypress/integration/links.test.js
@@ -1,16 +1,16 @@
 describe('Links', () => {
-    it('visits a different inertia page using the inertia-link component', () => {
-        cy.visit('/links')
+  it('visits a different inertia page using the inertia-link component', () => {
+    cy.visit('/links')
 
-        // Fail the assertion when a hard visit / location visit is made.
-        // Inertia's SPA-visit should not trigger this.
-        cy.on('load', () => expect(true).to.equal(false))
+    // Fail the assertion when a hard visit / location visit is made.
+    // Inertia's SPA-visit should not trigger this.
+    cy.on('load', () => expect(true).to.equal(false))
 
-        cy.get('[href="/links-target-1"]').click()
+    cy.get('[href="/links-target-1"]').click()
 
-        cy.url().should('eq', Cypress.config().baseUrl + '/links-target-1')
-        cy.get('body > div:first-child > span').should(($el) => {
-            expect($el.get(0).innerText).to.eq('This is one of the links target page')
-        })
+    cy.url().should('eq', Cypress.config().baseUrl + '/links-target-1')
+    cy.get('body > div:first-child > span').should(($el) => {
+      expect($el.get(0).innerText).to.eq('This is one of the links target page')
     })
+  })
 })
