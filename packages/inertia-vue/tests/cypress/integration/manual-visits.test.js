1d4400776fd854cc4052955b388199d59f492c9d
Claudio Dekker <1752195+claudiodekker@users.noreply.github.com>
2021-01-06 16:48:48 +0100

    Visits: Data (Object)

--- packages/inertia-vue/tests/cypress/integration/manual-visits.test.js
+++ packages/inertia-vue/tests/cypress/integration/manual-visits.test.js
@@ -101,4 +101,130 @@ describe('Manual Visits', () => {
         })
     })
   })
+
+  describe('Data', () => {
+    describe('plain objects', () => {
+      beforeEach(() => {
+        cy.visit('/visits/data/object', {
+          onLoad: () => cy.on('window:load', () => {
+            throw 'A location/non-SPA visit was detected'
+          }),
+        })
+      })
+
+      it('passes data as params using the visit method', () => {
+        cy.get('.visit').click()
+        cy.url().should('eq', Cypress.config().baseUrl + '/dump/get')
+
+        cy.window().should('have.property', '_inertia_request_dump')
+        cy.window()
+          .then(window => window._inertia_request_dump)
+          .then(({method, headers, form, files, query}) => {
+            expect(headers).to.contain.key('content-type')
+            expect(headers['content-type']).to.contain('application/json')
+
+            expect(method).to.eq('get')
+            expect(query).to.contain.key('foo')
+            expect(query.foo).to.eq('visit')
+            expect(form).to.be.empty
+            expect(files).to.be.empty
+          })
+      })
+
+      it('passes data as params using the GET method', () => {
+        cy.get('.get').click()
+        cy.url().should('eq', Cypress.config().baseUrl + '/dump/get')
+
+        cy.window().should('have.property', '_inertia_request_dump')
+        cy.window()
+          .then(window => window._inertia_request_dump)
+          .then(({method, headers, form, files, query}) => {
+            expect(headers).to.contain.key('content-type')
+            expect(headers['content-type']).to.contain('application/json')
+
+            expect(method).to.eq('get')
+            expect(query).to.contain.key('bar')
+            expect(query.bar).to.eq('get')
+            expect(form).to.be.empty
+            expect(files).to.be.empty
+          })
+      })
+
+      it('can pass data using the POST method', () => {
+        cy.get('.post').click()
+        cy.url().should('eq', Cypress.config().baseUrl + '/dump/post')
+
+        cy.window().should('have.property', '_inertia_request_dump')
+        cy.window()
+          .then(window => window._inertia_request_dump)
+          .then(({method, headers, form, files, query}) => {
+            expect(headers).to.contain.key('content-type')
+            expect(headers['content-type']).to.contain('application/json')
+
+            expect(method).to.eq('post')
+            expect(query).to.be.empty
+            expect(form).to.contain.key('baz')
+            expect(form.baz).to.eq('post')
+            expect(files).to.be.empty
+          })
+      })
+
+      it('can pass data using the PUT method', () => {
+        cy.get('.put').click()
+        cy.url().should('eq', Cypress.config().baseUrl + '/dump/put')
+
+        cy.window().should('have.property', '_inertia_request_dump')
+        cy.window()
+          .then(window => window._inertia_request_dump)
+          .then(({method, headers, form, files, query}) => {
+            expect(headers).to.contain.key('content-type')
+            expect(headers['content-type']).to.contain('application/json')
+
+            expect(method).to.eq('put')
+            expect(query).to.be.empty
+            expect(form).to.contain.key('foo')
+            expect(form.foo).to.eq('put')
+            expect(files).to.be.empty
+          })
+      })
+
+      it('can pass data using the PATCH method', () => {
+        cy.get('.patch').click()
+        cy.url().should('eq', Cypress.config().baseUrl + '/dump/patch')
+
+        cy.window().should('have.property', '_inertia_request_dump')
+        cy.window()
+          .then(window => window._inertia_request_dump)
+          .then(({method, headers, form, files, query}) => {
+            expect(headers).to.contain.key('content-type')
+            expect(headers['content-type']).to.contain('application/json')
+
+            expect(method).to.eq('patch')
+            expect(query).to.be.empty
+            expect(form).to.contain.key('bar')
+            expect(form.bar).to.eq('patch')
+            expect(files).to.be.empty
+          })
+      })
+
+      it('can pass data using the DELETE method', () => {
+        cy.get('.delete').click()
+        cy.url().should('eq', Cypress.config().baseUrl + '/dump/delete')
+
+        cy.window().should('have.property', '_inertia_request_dump')
+        cy.window()
+          .then(window => window._inertia_request_dump)
+          .then(({method, headers, form, files, query}) => {
+            expect(headers).to.contain.key('content-type')
+            expect(headers['content-type']).to.contain('application/json')
+
+            expect(method).to.eq('delete')
+            expect(query).to.be.empty
+            expect(form).to.contain.key('baz')
+            expect(form.baz).to.eq('delete')
+            expect(files).to.be.empty
+          })
+      })
+    })
+  })
 })
