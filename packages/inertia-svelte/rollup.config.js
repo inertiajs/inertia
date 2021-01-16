5d1dc792d173b8db368cc177bb51a5885beee828
Chris Toomey <chris@ctoomey.com>
2021-01-16 12:09:53 -0500

    Dont include peer dependencies

--- packages/inertia-svelte/rollup.config.js
+++ packages/inertia-svelte/rollup.config.js
@@ -1,8 +1,11 @@
 import json from '@rollup/plugin-json';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import svelte from 'rollup-plugin-svelte';
 import pkg from './package.json';
+
+const production = process.env.NODE_ENV !== 'development'
 
 export default [
   {
@@ -13,9 +16,17 @@ export default [
     ],
     plugins: [
       json(),
+      peerDepsExternal(),
+      svelte({
+        compilerOptions: {
+          dev: !production
+        }
+      }),
+      resolve({
+        browser: true,
+        dedupe: ['svelte/*']
+      }),
       commonjs(),
-      resolve({ browser: true }),
-      svelte()
     ]
   },
 ]
