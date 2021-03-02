36506e4996acd85ad112fbd80f7b2c0cb22230d8
mariojankovic <mario.jankovic@freshbooks.com>
2021-03-02 11:13:55 +0100

    Replace nested hack with render component

---
+++ packages/inertia-svelte/src/Render.svelte
@@ -0,0 +1,16 @@
+<script>
+  import store from './store'
+
+  export let index = 0
+  $: component = $store.layout[index]
+    ? $store.layout[index]
+    : $store.component.default
+</script>
+
+{#key $store.key}
+  <svelte:component this={component} {...$store.page.props}>
+    {#each $store.layout as layout, childIndex}
+      <svelte:self {layout} index={childIndex + 1} />
+    {/each}
+  </svelte:component>
+{/key}
