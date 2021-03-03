b40c05734c41974cd6bf95b04f92906d3e868272
mariojankovic <mario.jankovic@freshbooks.com>
2021-03-03 11:48:57 +0100

    Fixes peer deps and introduces terser.

--- packages/inertia-svelte/rollup.config.js
+++ packages/inertia-svelte/rollup.config.js
@@ -1,32 +1,38 @@
-import json from '@rollup/plugin-json';
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import svelte from 'rollup-plugin-svelte';
-import pkg from './package.json';
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import svelte from 'rollup-plugin-svelte'
+import { terser } from 'rollup-plugin-terser'
+import pkg from './package.json'
 
-const production = process.env.NODE_ENV !== 'development'
+const production = !process.env.ROLLUP_WATCH
 
-export default [
-  {
-    input: 'src/index.js',
-    output: [
-      { file: pkg.module, 'format': 'es' },
-      { file: pkg.main, 'format': 'umd', name: 'inertia-svelte' }
-    ],
-    plugins: [
-      json(),
-      peerDepsExternal(),
-      svelte({
-        compilerOptions: {
-          dev: !production
-        }
-      }),
-      resolve({
-        browser: true,
-        dedupe: ['svelte/*']
-      }),
-      commonjs(),
-    ]
-  },
-]
+const globals = {
+  svelte: 'Svelte',
+  'svelte/internal': 'internal',
+  'svelte/store': 'store',
+  '@inertiajs/inertia': 'Inertia',
+}
+
+export default {
+  input: 'src/index.js',
+  external: ['svelte', '@inertiajs/inertia'],
+  output: [
+    { file: pkg.module, format: 'es', globals },
+    { file: pkg.main, format: 'umd', name: 'inertia-svelte', globals },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    svelte({
+      compilerOptions: {
+        dev: !production,
+      },
+    }),
+    resolve({
+      browser: true,
+      dedupe: ['svelte'],
+    }),
+    commonjs(),
+    production && terser(),
+  ],
+}
