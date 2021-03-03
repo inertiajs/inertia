b846748b8c918a5b6e0bf79dbc3ec45d67bc0048
Mario Jankovic <majankovi@gmail.com>
2021-03-03 15:57:29 +0100

    Replace nested hack with render component (#529)
    * Replace nested hack with render component
    
    * Adds key to nested item instead.
    
    * Removes layout prop.
    
    * Fixes #285
    
    * Reverts and fixes previous nesting.
    
    * Adds key to child component.
    
    * Updates key value for last child only.
    
    Co-authored-by: mariojankovic <mario.jankovic@freshbooks.com>

--- packages/inertia-svelte/src/App.svelte
+++ packages/inertia-svelte/src/App.svelte
@@ -1,12 +1,9 @@
 <script>
   import { Inertia } from '@inertiajs/inertia'
   import store from './store'
+  import Render, { h } from './Render.svelte'
 
-  export let
-    initialPage,
-    resolveComponent,
-    resolveErrors,
-    transformProps
+  export let initialPage, resolveComponent, resolveErrors, transformProps
 
   Inertia.init({
     initialPage,
@@ -14,69 +11,24 @@
     resolveErrors,
     transformProps,
     swapComponent: async ({ component, page, preserveState }) => {
-      store.update(current => ({
+      store.update((current) => ({
         component,
-        layout: (() => {
-          if (!component.layout) {
-            return []
-          } else if (Array.isArray(component.layout)) {
-            if (component.layout.length > 5) {
-              throw new Error('The Svelte adapter only supports up to five nested layouts.')
-            }
-            return component.layout
-          } else {
-            return [component.layout]
-          }
-        })(),
         page,
-        key: preserveState ? current.key : Date.now(),
+        key: preserveState ? current.key : Date.now()
       }))
-    },
+    }
   })
+
+  $: child = $store.component && h($store.component.default, $store.page.props)
+  $: layout = $store.component && $store.component.layout
+  $: components = layout
+    ? Array.isArray(layout)
+      ? layout
+          .concat(child)
+          .reverse()
+          .reduce((child, layout) => h(layout, {}, [child]))
+      : h(layout, {}, [child])
+    : child
 </script>
 
-{#if $store.component}
-  {#if $store.layout.length > 0}
-    <svelte:component this={$store.layout[0]}>
-      {#if $store.layout.length > 1}
-        <svelte:component this={$store.layout[1]}>
-          {#if $store.layout.length > 2}
-            <svelte:component this={$store.layout[2]}>
-              {#if $store.layout.length > 3}
-                <svelte:component this={$store.layout[3]}>
-                  {#if $store.layout.length > 4}
-                    <svelte:component this={$store.layout[4]}>
-                      {#key $store.key}
-                        <svelte:component this={$store.component.default} {...$store.page.props} />
-                      {/key}
-                    </svelte:component>
-                  {:else}
-                    {#key $store.key}
-                      <svelte:component this={$store.component.default} {...$store.page.props} />
-                    {/key}
-                  {/if}
-                </svelte:component>
-              {:else}
-                {#key $store.key}
-                  <svelte:component this={$store.component.default} {...$store.page.props} />
-                {/key}
-              {/if}
-            </svelte:component>
-          {:else}
-            {#key $store.key}
-              <svelte:component this={$store.component.default} {...$store.page.props} />
-            {/key}
-          {/if}
-        </svelte:component>
-      {:else}
-        {#key $store.key}
-          <svelte:component this={$store.component.default} {...$store.page.props} />
-        {/key}
-      {/if}
-    </svelte:component>
-  {:else}
-    {#key $store.key}
-      <svelte:component this={$store.component.default} {...$store.page.props} />
-    {/key}
-  {/if}
-{/if}
+<Render {...components} />
