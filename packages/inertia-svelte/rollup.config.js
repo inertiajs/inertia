36f51f646ff3e1319a2f47e1bfe24cd20338da7a
mariojankovic <mario.jankovic@freshbooks.com>
2021-03-03 14:20:24 +0100

    Adds sourcemaps.

--- packages/inertia-svelte/rollup.config.js
+++ packages/inertia-svelte/rollup.config.js
@@ -1,14 +1,15 @@
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
-import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import svelte from 'rollup-plugin-svelte'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import babel from 'rollup-plugin-babel'
 import { terser } from 'rollup-plugin-terser'
 import pkg from './package.json'
 
 const production = !process.env.ROLLUP_WATCH
 
 const globals = {
-  svelte: 'Svelte',
+  svelte: 'svelte',
   'svelte/internal': 'internal',
   'svelte/store': 'store',
   '@inertiajs/inertia': 'Inertia',
@@ -18,8 +19,8 @@ export default {
   input: 'src/index.js',
   external: ['svelte', '@inertiajs/inertia'],
   output: [
-    { file: pkg.module, format: 'es', globals },
-    { file: pkg.main, format: 'umd', name: 'inertia-svelte', globals },
+    { file: pkg.module, format: 'es', globals, sourcemap: true },
+    { file: pkg.main, format: 'umd', name: 'inertia-svelte', globals, sourcemap: true },
   ],
   plugins: [
     peerDepsExternal(),
@@ -33,6 +34,7 @@ export default {
       dedupe: ['svelte'],
     }),
     commonjs(),
+    babel(),
     production && terser(),
   ],
 }
