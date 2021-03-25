e3c1890c70893f74860735abe3ac3ef37032f691
Jonathan Reinink <jonathan@reinink.ca>
2021-03-25 10:08:57 -0400

    Rename "remember" to "useRemember"

--- packages/inertia-svelte/src/index.js
+++ packages/inertia-svelte/src/index.js
@@ -2,5 +2,5 @@ export { default as App, default as app, default as InertiaApp } from './App.sve
 export { default as Link, default as link, default as InertiaLink } from './InertiaLink.svelte'
 export { default as inertia } from './link'
 export { default as page } from './page'
-export { default as remember } from './remember'
+export { default as useRemember, default as remember } from './useRemember'
 export { default as useForm } from './useForm'
