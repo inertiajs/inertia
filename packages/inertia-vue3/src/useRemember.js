af907dfa646308b42d03ff9d8d31e6f458256d98
Jonathan Reinink <jonathan@reinink.ca>
2021-03-25 18:54:22 -0400

    Update Vue 3 remember behaviour to match ref/reactive type

--- packages/inertia-vue3/src/useRemember.js
+++ packages/inertia-vue3/src/useRemember.js
@@ -1,4 +1,4 @@
-import { ref, toRaw, unref, watch } from 'vue'
+import { isReactive, reactive, ref, toRaw, unref, watch } from 'vue'
 import { Inertia } from '@inertiajs/inertia'
 
 export default function useRemember(data, key) {
@@ -7,7 +7,7 @@ export default function useRemember(data, key) {
   }
 
   const restored = Inertia.restore(key)
-  const remembered = restored === undefined ? data : ref(restored)
+  const remembered = restored === undefined ? data : (isReactive(data) ? reactive(data) : ref(restored))
   watch(remembered, (value) => Inertia.remember(toRaw(unref(value)), key), { immediate: true, deep: true })
 
   return remembered
