0ce3fa013d7afa7b8efd5327166703326aab5dec
Jonathan Reinink <jonathan@reinink.ca>
2021-03-25 17:23:22 -0400

    Add "rememberable" check to Vue 3 useRemember hook

--- packages/inertia-vue3/src/useRemember.js
+++ packages/inertia-vue3/src/useRemember.js
@@ -2,6 +2,10 @@ import { ref, toRaw, unref, watch } from 'vue'
 import { Inertia } from '@inertiajs/inertia'
 
 export default function useRemember(data, key) {
+  if (typeof data === 'object' && data !== null && data.__rememberable === false) {
+    return data
+  }
+
   const restored = Inertia.restore(key)
   const remembered = restored === undefined ? data : ref(restored)
   watch(remembered, (value) => Inertia.remember(toRaw(unref(value)), key), { immediate: true, deep: true })
