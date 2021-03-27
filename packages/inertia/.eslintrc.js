744cf17dd62ba55e446e6c9818959874a7649945
Claudio Dekker <1752195+claudiodekker@users.noreply.github.com>
2021-03-27 16:06:05 +0100

    Set up TypeScript

--- packages/inertia/.eslintrc.js
+++ packages/inertia/.eslintrc.js
@@ -3,11 +3,18 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  extends: ['eslint:recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
   },
+  plugins: [
+    '@typescript-eslint',
+  ],
   rules: {
     indent: ['error', 2],
     quotes: ['warn', 'single'],
