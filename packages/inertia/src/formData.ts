214031029fefd18ccf9855f6f77647f543b3115d
Claudio Dekker <1752195+claudiodekker@users.noreply.github.com>
2021-03-28 17:33:34 +0200

    Migrate formData.js

---
+++ packages/inertia/src/formData.ts
@@ -0,0 +1,45 @@
+export type FormDataConvertible = Date|File|Blob|boolean|string|number|null|undefined
+
+export function objectToFormData(
+  source: Record<string, FormDataConvertible>|null|undefined,
+  form: FormData = new FormData(),
+  parentKey: string|null = null,
+): FormData {
+  source = source || {}
+
+  for (const key in source) {
+    if (Object.prototype.hasOwnProperty.call(source, key)) {
+      append(form, composeKey(parentKey, key), source[key])
+    }
+  }
+
+  return form
+}
+
+function composeKey(parent: string|null, key: string) : string {
+  return parent ? parent + '[' + key + ']' : key
+}
+
+function append(
+  form: FormData,
+  key: string,
+  value: FormDataConvertible|Record<string, FormDataConvertible>,
+): void {
+  if (value instanceof Date) {
+    return form.append(key, value.toISOString())
+  } else if (value instanceof File) {
+    return form.append(key, value, value.name)
+  } else if (value instanceof Blob) {
+    return form.append(key, value)
+  } else if (typeof value === 'boolean') {
+    return form.append(key, value ? '1' : '0')
+  } else if (typeof value === 'string') {
+    return form.append(key, value)
+  } else if (typeof value === 'number') {
+    return form.append(key, `${value}`)
+  } else if (value === null || value === undefined) {
+    return form.append(key, '')
+  }
+
+  objectToFormData(value, form, key)
+}
