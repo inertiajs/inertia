e17cbbd504e9e2b03bb383ccfbfc002b3720a54c
Claudio Dekker <1752195+claudiodekker@users.noreply.github.com>
2021-03-29 13:31:31 +0200

    Fix debounce

--- packages/inertia/src/debounce.ts
+++ packages/inertia/src/debounce.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/ban-types
-export default function debounce(fn: Function, delay: number): CallableFunction {
+export default function debounce(fn: Function, delay: number): VoidFunction {
   let timeoutID: NodeJS.Timeout
-  return function (... args: unknown[]) {
+  return function (... args: unknown[]): void {
     clearTimeout(timeoutID)
     timeoutID = setTimeout(
       () => fn.apply(this, args),
