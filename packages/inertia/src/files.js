f1828677e31db99568ca89097877e1c266b8fed8
Jonathan Reinink <jonathan@reinink.ca>
2021-05-18 08:50:22 -0400

    Formatting

--- packages/inertia/src/files.js
+++ packages/inertia/src/files.js
@@ -2,6 +2,6 @@ export function hasFiles(data) {
   return data instanceof File
     || data instanceof Blob
     || data instanceof FileList
-    || (typeof data === 'object' && data !== null && Object.values(data).find(value => hasFiles(value)) !== undefined)
     || data instanceof FormData && Array.from(data.values()).some(value => hasFiles(value))
+    || (typeof data === 'object' && data !== null && Object.values(data).find(value => hasFiles(value)) !== undefined)
 }
