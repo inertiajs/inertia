9cf95490734f370e59035abcbc84bcfe64b7af12
Jonathan Reinink <jonathan@reinink.ca>
2021-05-21 11:13:09 -0400

    Improve mergeDataIntoQueryString function

--- packages/inertia/src/url.ts
+++ packages/inertia/src/url.ts
@@ -8,9 +8,20 @@ export function hrefToUrl(href: string|URL): URL {
 
 export function mergeDataIntoQueryString(
   method: Method,
-  url: URL,
+  href: URL|string,
   data: Record<string, FormDataConvertible>,
-): [URL, Record<string, FormDataConvertible>] {
+): {
+  href: string,
+  data: Record<string, FormDataConvertible>
+} {
+  const hasHost = href.toString().includes('http')
+  const hasAbsolutePath = hasHost || href.toString().startsWith('/')
+  const hasRelativePath = !hasAbsolutePath && !href.toString().startsWith('#') && !href.toString().startsWith('?')
+  const hasSearch = href.toString().includes('?') || (method === Method.GET && Object.keys(data).length)
+  const hasHash = href.toString().includes('#')
+
+  const url = new URL(href.toString(), 'http://localhost')
+
   if (method === Method.GET && Object.keys(data).length) {
     url.search = qs.stringify(
       deepmerge(qs.parse(url.search, { ignoreQueryPrefix: true }), data), {
@@ -20,10 +31,17 @@ export function mergeDataIntoQueryString(
     )
     data = {}
   }
-  return [
-    url,
+
+  return {
+    href: [
+      hasHost ? `${url.protocol}//${url.host}` : '',
+      hasAbsolutePath ? url.pathname : '',
+      hasRelativePath ? url.pathname.substring(1) : '',
+      hasSearch ? url.search : '',
+      hasHash ? url.hash : '',
+    ].join(''),
     data,
-  ]
+  }
 }
 
 export function urlWithoutHash(url: URL|Location): URL {
