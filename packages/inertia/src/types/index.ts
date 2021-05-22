756f70f8e4801fb86b28c3329c5ee26cea0250ca
Claudio Dekker <1752195+claudiodekker@users.noreply.github.com>
2021-05-22 20:55:37 +0200

    (Attempt to) improve type support

--- packages/inertia/src/types/index.ts
+++
@@ -1,4 +0,0 @@
-export { Page } from './page'
-export { ErrorResolver, PageHandler, PageResolver, PropTransformer } from './adapter'
-export { Errors, ErrorBag, FormDataConvertible, Method, RequestPayload } from './request'
-export { ActiveVisit, LocationVisit, PreserveStateOption, Visit, VisitId } from './visit'
