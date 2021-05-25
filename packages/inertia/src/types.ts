cdc7bc6f8c3ddda40b8613b417035a654f63f29f
Jonathan Reinink <jonathan@reinink.ca>
2021-05-25 12:18:32 -0400

    Formatting

--- packages/inertia/src/types.ts
+++ packages/inertia/src/types.ts
@@ -6,69 +6,69 @@ export type ErrorBag = Record<string, Errors>
 export type FormDataConvertible = Date|File|Blob|boolean|string|number|null|undefined
 
 export enum Method {
-    GET = 'get',
-    POST = 'post',
-    PUT = 'put',
-    PATCH = 'patch',
-    DELETE = 'delete',
+  GET = 'get',
+  POST = 'post',
+  PUT = 'put',
+  PATCH = 'patch',
+  DELETE = 'delete',
 }
 
 export type RequestPayload = Record<string, FormDataConvertible>|FormData
 
 export interface Page {
-    component: string,
-    props: {
-        [key: string]: unknown,
-        errors: Errors & ErrorBag,
-    }
-    url: string,
-    version: string|null
+  component: string,
+  props: {
+    [key: string]: unknown,
+    errors: Errors & ErrorBag,
+  }
+  url: string,
+  version: string|null
 
-    // Refactor away
-    scrollRegions: Array<{ top: number, left: number }>
-    rememberedState: Record<string, unknown>
-    resolvedErrors: Errors
+  // Refactor away
+  scrollRegions: Array<{ top: number, left: number }>
+  rememberedState: Record<string, unknown>
+  resolvedErrors: Errors
 }
 
 export type PreserveStateOption = boolean|string|((page: Page) => boolean)
 
 export type LocationVisit = {
-    preserveScroll: boolean,
+  preserveScroll: boolean,
 }
 
 export interface Visit {
-    url: URL,
-    method: Method,
-    data: Record<string, unknown>|FormData,
-    replace: boolean,
-    preserveScroll: PreserveStateOption,
-    preserveState: PreserveStateOption,
-    only: Array<string>,
-    headers: Record<string, string>
-    errorBag: string|null,
-    forceFormData: boolean,
+  url: URL,
+  method: Method,
+  data: Record<string, unknown>|FormData,
+  replace: boolean,
+  preserveScroll: PreserveStateOption,
+  preserveState: PreserveStateOption,
+  only: Array<string>,
+  headers: Record<string, string>
+  errorBag: string|null,
+  forceFormData: boolean,
 
-    completed: boolean,
-    cancelled: boolean,
-    interrupted: boolean,
+  completed: boolean,
+  cancelled: boolean,
+  interrupted: boolean,
 }
 
 export interface LocalEventCallbacks {
-    onCancelToken?: { ({ cancel }: { cancel: VoidFunction }): void },
-    onBefore: (visit: Visit) => boolean|void,
-    onStart: (visit: Visit) => void,
-    onProgress: (progress: { percentage: number }|void) => void,
-    onFinish: (visit: Visit) => void,
-    onCancel: () => void,
-    onBeforeRender?: (page: Page) => void,
-    onSuccess?: (page: Page) => void,
-    onError?: (errors: Errors) => void,
+  onCancelToken?: { ({ cancel }: { cancel: VoidFunction }): void },
+  onBefore: (visit: Visit) => boolean|void,
+  onStart: (visit: Visit) => void,
+  onProgress: (progress: { percentage: number }|void) => void,
+  onFinish: (visit: Visit) => void,
+  onCancel: () => void,
+  onBeforeRender?: (page: Page) => void,
+  onSuccess?: (page: Page) => void,
+  onError?: (errors: Errors) => void,
 }
 
 export type VisitOptions = Visit & LocalEventCallbacks
 
 export interface ActiveVisit extends VisitOptions {
-    cancelToken: CancelTokenSource,
+  cancelToken: CancelTokenSource,
 }
 
 export type VisitId = unknown
@@ -82,7 +82,7 @@ export type PageHandler = ({
   page,
   preserveState,
 }: {
-    component: Component,
-    page: Page,
-    preserveState: PreserveStateOption,
+  component: Component,
+  page: Page,
+  preserveState: PreserveStateOption,
 }) => Promise<unknown>
