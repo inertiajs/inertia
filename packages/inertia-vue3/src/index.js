14e5f7d98889a1cd169e6a407e3d41c90333a3fa
Jonathan Reinink <jonathan@reinink.ca>
2021-05-28 12:17:07 -0400

    Add createInertiaApp() method to Vue 3 adapter

--- packages/inertia-vue3/src/index.js
+++ packages/inertia-vue3/src/index.js
@@ -1,4 +1,5 @@
 export { default as useForm } from './useForm'
 export { default as useRemember } from './useRemember'
 export { default as Link, default as link } from './link'
+export { default as createInertiaApp } from './createInertiaApp'
 export { default as App, default as app, plugin, usePage } from './app'
