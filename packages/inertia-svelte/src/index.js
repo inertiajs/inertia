49b0e8c80f83efb39758450cc5939b7c744966a4
Jonathan Reinink <jonathan@reinink.ca>
2021-05-29 07:19:29 -0400

    Add createInertiaApp() method to Svelte adapter

--- packages/inertia-svelte/src/index.js
+++ packages/inertia-svelte/src/index.js
@@ -1,6 +1,7 @@
 export { default as page } from './page'
 export { default as inertia } from './link'
 export { default as useForm } from './useForm'
+export { default as createInertiaApp } from './createInertiaApp'
 export { default as useRemember, default as remember } from './useRemember'
 export { default as App, default as app, default as InertiaApp } from './App.svelte'
 export { default as Link, default as link, default as InertiaLink } from './InertiaLink.svelte'
