a5c154008b3675e54bf23515255e97d40b236149
Julian Hundeloh <jaulz@users.noreply.github.com>
2021-06-03 13:35:53 +0200

    feat: expose types

--- packages/inertia/src/index.ts
+++ packages/inertia/src/index.ts
@@ -4,4 +4,6 @@ export { default as createHeadManager } from './head'
 export { default as shouldIntercept } from './shouldIntercept'
 export { hrefToUrl, mergeDataIntoQueryString, urlWithoutHash } from './url'
 
+export * from './types'
+
 export const Inertia = new Router()
