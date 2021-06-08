bd761d8013ac517c527dd77b8cc21754bc3490ac
Jonathan Reinink <jonathan@reinink.ca>
2021-06-08 16:22:19 -0400

    Switch to render() function when rendering head in Vue 3

--- packages/inertia-vue3/src/head.js
+++ packages/inertia-vue3/src/head.js
@@ -1,3 +1,5 @@
+import { h, render } from 'vue'
+
 export default {
   props: {
     title: {
@@ -14,68 +16,27 @@ export default {
     this.provider.disconnect()
   },
   methods: {
-    isUnaryTag(node) {
-      return [
-        'area', 'base', 'br', 'col', 'embed', 'hr', 'img',
-        'input', 'keygen', 'link', 'meta', 'param', 'source',
-        'track', 'wbr',
-      ].indexOf(node.type) > -1
-    },
-    renderTagStart(node) {
-      node.props = node.props || {}
-      node.props.inertia = node.props['head-key'] !== undefined ? node.props['head-key'] : ''
-      const attrs = Object.keys(node.props).reduce((carry, name) => {
-        const value = node.props[name]
-        if (['key', 'head-key'].includes(name)) {
-          return carry
-        } else if (value === '') {
-          return carry + ` ${name}`
-        } else {
-          return carry + ` ${name}="${value}"`
-        }
-      }, '')
-      return `<${node.type}${attrs}>`
-    },
-    renderTagChildren(node) {
-      return typeof node.children === 'string'
-        ? node.children
-        : node.children.reduce((html, child) => html + this.renderTag(child), '')
-    },
-    renderTag(node) {
-      if (node.type.toString() === 'Symbol(Text)') {
-        return node.children
-      } else if (node.type.toString() === 'Symbol()') {
-        return ''
-      } else if (node.type.toString() === 'Symbol(Comment)') {
-        return ''
-      }
-      let html = this.renderTagStart(node)
-      if (node.children) {
-        html += this.renderTagChildren(node)
-      }
-      if (!this.isUnaryTag(node)) {
-        html += `</${node.type}>`
-      }
-      return html
-    },
     addTitleElement(elements) {
       if (this.title && !elements.find(tag => tag.startsWith('<title'))) {
         elements.push(`<title inertia>${this.title}</title>`)
       }
       return elements
     },
-    renderNodes(nodes) {
-      return this.addTitleElement(
-        nodes
-          .flatMap(node => node.type.toString() === 'Symbol(Fragment)' ? node.children : node)
-          .map(node => this.renderTag(node))
-          .filter(node => node)
-      )
-    },
   },
   render() {
+    const el = document.createElement('div')
+
+    render(h('div', this.$slots.default ? this.$slots.default() : []), el)
+
+    const elements = Array.from(el.firstChild.childNodes)
+      .filter(node => node.outerHTML)
+      .map(node => {
+        node.setAttribute('inertia', node.getAttribute('head-key') || '')
+        return node.outerHTML
+      })
+
     this.provider.update(
-      this.renderNodes(this.$slots.default ? this.$slots.default() : [])
+      this.addTitleElement(elements)
     )
   },
 }
