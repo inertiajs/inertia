09b021973a595941fed89e9f0ef107ef985cf015
Jonathan Reinink <jonathan@reinink.ca>
2021-06-21 19:14:04 -0400

    Remove component registration from Vue 2 adapter

--- packages/inertia-vue/src/index.js
+++ packages/inertia-vue/src/index.js
@@ -1,3 +1,4 @@
 export { default as createInertiaApp } from './createInertiaApp'
+export { default as Head, default as head, default as InertiaHead } from './head'
 export { default as Link, default as link, default as InertiaLink } from './link'
 export { default as App, default as app, plugin, default as InertiaApp } from './app'
