513e70db511f887e7e131cf05cc6e999690866e7
Jonathan Reinink <jonathan@reinink.ca>
2021-06-22 07:27:12 -0400

    Remove duplicate exports from Vue 2 adapter

--- packages/inertia-vue/src/index.js
+++ packages/inertia-vue/src/index.js
@@ -1,4 +1,4 @@
+export { default as Head } from './head'
+export { default as Link } from './link'
+export { default as App, plugin } from './app'
 export { default as createInertiaApp } from './createInertiaApp'
-export { default as Head, default as head, default as InertiaHead } from './head'
-export { default as Link, default as link, default as InertiaLink } from './link'
-export { default as App, default as app, plugin, default as InertiaApp } from './app'
