1e87dea72816ac77178ca523a8562701ee2651ce
Claudio Dekker <1752195+claudiodekker@users.noreply.github.com>
2021-06-24 20:18:49 +0200

    WIP

--- packages/inertia-vue/src/types.ts
+++ packages/inertia-vue/src/types.ts
@@ -1,8 +1,7 @@
 import { Router } from '@inertiajs/inertia/types/router'
-import {HeadManager, Page} from '@inertiajs/inertia/types/types'
+import { HeadManager, Page } from '@inertiajs/inertia/types/types'
 
 declare module 'vue/types/vue' {
-    // 3. Declare augmentation for Vue
     interface Vue {
         $headManager?: HeadManager
         $inertia: Router,
