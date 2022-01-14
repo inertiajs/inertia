acdd0e4a0926c707a9a41e340c8c599a82d313d1
Enzo Innocenzi <enzo@innocenzi.dev>
2022-01-14 20:19:50 +0100

    feat(link): support `disabled` property (#926)

--- packages/inertia-svelte/src/link.js
+++ packages/inertia-svelte/src/link.js
@@ -9,6 +9,10 @@ export default (node, options = {}) => {
   const dispatch = createEventDispatcher()
 
   function visit(event) {
+    if (node.tagName === 'BUTTON' && node.disabled || options.disabled) {
+      return
+    }
+
     dispatch('click', event)
 
     const href = node.href || options.href
