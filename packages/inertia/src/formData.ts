b32dfe98d15f193237276129cc61620a702b1036
Adrien Foulon <6115458+Tofandel@users.noreply.github.com>
2022-03-07 22:52:33 +0100

    fix: Blob can have name as well

--- packages/inertia/src/formData.ts
+++ packages/inertia/src/formData.ts
@@ -25,10 +25,8 @@ function append(form: FormData, key: string, value: FormDataConvertible): void {
     return Array.from(value.keys()).forEach(index => append(form, composeKey(key, index.toString()), value[index]))
   } else if (value instanceof Date) {
     return form.append(key, value.toISOString())
-  } else if (value instanceof File) {
+  } else if (value instanceof File || value instanceof Blob) {
     return form.append(key, value, value.name)
-  } else if (value instanceof Blob) {
-    return form.append(key, value)
   } else if (typeof value === 'boolean') {
     return form.append(key, value ? '1' : '0')
   } else if (typeof value === 'string') {
