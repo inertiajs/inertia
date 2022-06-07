496047bd1afb150a18f75318ffa53a0c39a2daa9
Lucas Yang <yangchenshin77@gmail.com>
2022-06-07 22:52:12 +0800

    Rename types file to types.ts & remove comments

--- packages/inertia-vue3/src/index.ts
+++ packages/inertia-vue3/src/index.ts
@@ -5,4 +5,4 @@ export { default as Head, default as InertiaHead } from './head'
 export { default as Link, default as InertiaLink, default as link } from './link'
 export { App, App as InertiaApp, App as app, plugin, usePage } from './app'
 
-export * from './globalExtensions'
+export * from './types'
--- packages/inertia-vue3/src/types.ts
+++ packages/inertia-vue3/src/types.ts
@@ -4,36 +4,16 @@ import { FormFactory } from './form'
 
 declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
-    /**
-     * Inertia router instance.
-     */
     $inertia: Omit<typeof Inertia, 'form'> & { form: FormFactory }
-
-    /**
-     * Current page infomation.
-     */
     $page: Page
-
-    /**
-     * Inertia page head manager.
-     */
     $headManager: HeadManager
   }
 
   interface ComponentCustomOptions {
-    /**
-     * Define layout for current page.
-     */
     layout?: LayoutComponent
-
-    /**
-     * Remember data.
-     */
     remember?: string | string[] | {
       data: string[]
       key?: string | (() => string)
     }
   }
 }
-
-export {}
