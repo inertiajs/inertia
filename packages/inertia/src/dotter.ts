47bd54b01e95e47d9941a9bbc3a33973375780c4
Tofandel <adrien.foulon@tukan.hu>
2022-08-07 03:17:00 +0200

    Use dot split instead

--- packages/inertia/src/dotter.ts
+++ packages/inertia/src/dotter.ts
@@ -1,10 +1,12 @@
+// This is basically split('.') but more thorough as it only splits valid dot notations and prevents splitting things like `foo...bar`
+const dotSplit = (str: string) => str.match(/^\.+[^.]*|[^.]*\.+$|(?:\.{2,}|[^.])+(?:\.+$)?/g) || [str]
+
 export const dotGet = (str: string, obj: Record<string, unknown>): unknown =>
-  str
-    .split('.')
+  dotSplit(str)
     .reduce((obj: unknown, i): unknown => typeof obj === 'object' && obj !== null ? (obj as Record<string, unknown>)[i] : undefined, obj)
 
 export const dotSet = (target: Record<string, unknown>, exp: string, value: unknown): void => {
-  const levels = exp.split('.')
+  const levels = dotSplit(exp)
   const max_level = levels.length - 1
   levels.forEach(function (level, i) {
     if (i === max_level) {
