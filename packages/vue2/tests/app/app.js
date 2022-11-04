c31fb4a61d250a98cd5a31e5e3a6edc955e33594
Jonathan Reinink <jonathan@reinink.ca>
2022-11-03 22:19:01 -0400

    Update test suite to use `createInertiaApp()`

--- packages/vue2/tests/app/app.js
+++ packages/vue2/tests/app/app.js
@@ -1,30 +1,27 @@
+import { createInertiaApp, Link, router } from '@inertiajs/vue2'
 import Vue from 'vue'
-import { router, App, Link, plugin } from '@inertiajs/vue2'
-
-if (!window.location.pathname.startsWith('/plugin/without')) {
-  Vue.use(plugin)
-}
 
 Vue.component('InertiaLink', Link)
 
-const app = document.getElementById('app')
-
 window.testing = {}
 window.testing.Inertia = router
-window.testing.vue = new Vue({
-  render: (h) =>
-    h(App, {
-      props: {
-        initialPage: window.initialPage,
-        resolveComponent: (name) => {
-          return import(`./Pages/${name}`).then((module) => module.default)
+
+createInertiaApp({
+  page: window.initialPage,
+  resolve: (name) => import(`./Pages/${name}`),
+  setup({ el, App, props, plugin }) {
+    if (!window.location.pathname.startsWith('/plugin/without')) {
+      Vue.use(plugin)
+    }
+
+    window.testing.vue = new Vue({
+      render: (h) => h(App, props),
+      methods: {
+        tap: (value, callback) => {
+          callback(value)
+          return value
         },
       },
-    }),
-  methods: {
-    tap: (value, callback) => {
-      callback(value)
-      return value
-    },
+    }).$mount(el)
   },
-}).$mount(app)
+})
