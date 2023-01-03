6de0318e296550de5324c523dfb5e69e0f8c076b
Jess Archer <jess@jessarcher.com>
2023-01-04 00:10:33 +1000

    Add interface for Vue 3 useForm

--- packages/vue3/src/useForm.ts
+++ packages/vue3/src/useForm.ts
@@ -1,12 +1,50 @@
-import { router, VisitOptions } from '@inertiajs/core'
+import { Progress, router, VisitOptions } from '@inertiajs/core'
 import cloneDeep from 'lodash.clonedeep'
 import isEqual from 'lodash.isequal'
 import { reactive, watch } from 'vue'
 
-export default function useForm(...args) {
-  const rememberKey = typeof args[0] === 'string' ? args[0] : null
-  const data = (typeof args[0] === 'string' ? args[1] : args[0]) || {}
-  const restored = rememberKey ? (router.restore(rememberKey) as { data: any; errors: any }) : null
+interface InertiaFormProps<TForm extends Record<string, unknown>> {
+  isDirty: boolean
+  errors: Partial<Record<keyof TForm, string>>
+  hasErrors: boolean
+  processing: boolean
+  progress: Progress | null
+  wasSuccessful: boolean
+  recentlySuccessful: boolean
+  data(): TForm
+  transform(callback: (data: TForm) => object): this
+  defaults(): this
+  defaults(field: keyof TForm, value: string): this
+  defaults(fields: Record<keyof TForm, string>): this
+  reset(...fields: (keyof TForm)[]): this
+  clearErrors(...fields: (keyof TForm)[]): this
+  setError(field: keyof TForm, value: string): this
+  setError(errors: Record<keyof TForm, string>): this
+  submit(method: string, url: string, options?: Partial<VisitOptions>): void
+  get(url: string, options?: Partial<VisitOptions>): void
+  post(url: string, options?: Partial<VisitOptions>): void
+  put(url: string, options?: Partial<VisitOptions>): void
+  patch(url: string, options?: Partial<VisitOptions>): void
+  delete(url: string, options?: Partial<VisitOptions>): void
+  cancel(): void
+}
+
+type InertiaForm<TForm extends Record<string, unknown>> = TForm & InertiaFormProps<TForm>
+
+export default function useForm<TForm extends Record<string, unknown>>(data: TForm): InertiaForm<TForm>
+export default function useForm<TForm extends Record<string, unknown>>(
+  rememberKey: string,
+  data: TForm,
+): InertiaForm<TForm>
+export default function useForm<TForm extends Record<string, unknown>>(
+  rememberKeyOrData: string | TForm,
+  maybeData?: TForm,
+) {
+  const rememberKey = typeof rememberKeyOrData === 'string' ? rememberKeyOrData : null
+  const data = typeof rememberKeyOrData === 'object' ? rememberKeyOrData : (maybeData as TForm)
+  const restored = rememberKey
+    ? (router.restore(rememberKey) as { data: TForm; errors: Record<keyof TForm, string> })
+    : null
   let defaults = cloneDeep(data)
   let cancelToken = null
   let recentlySuccessfulTimeoutId = null
