f711b467cc21ec2e5cb1ada9ccb3b9049fc23231
Jonathan Reinink <jonathan@reinink.ca>
2023-01-02 22:44:21 -0500

    Convert core to use esbuild

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -5,5 +5,6 @@ export { default as setupProgress } from './progress'
 export { default as shouldIntercept } from './shouldIntercept'
 export * from './types'
 export { hrefToUrl, mergeDataIntoQueryString, urlWithoutHash } from './url'
+export { type Router }
 
 export const router = new Router()
