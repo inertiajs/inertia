87edfb2fe3b5a5e554ff47124f654ccce5da9f58
Jonathan Reinink <jonathan@reinink.ca>
2023-01-03 23:57:54 -0500

    Remove `defineLayout()` from Vue 3 playground

--- playgrounds/vue3/resources/js/Pages/Home.vue
+++ playgrounds/vue3/resources/js/Pages/Home.vue
@@ -1,10 +1,9 @@
 <script setup>
-import { defineLayout } from '@inertiajs/vue3'
 import Layout from '../Components/Layout.vue'
-
-defineLayout(Layout)
 </script>
 
 <template>
-  <h1 class="text-3xl">Home</h1>
+  <Layout>
+    <h1 class="text-3xl">Home</h1>
+  </Layout>
 </template>
--- playgrounds/vue3/resources/js/Pages/Users.vue
+++ playgrounds/vue3/resources/js/Pages/Users.vue
@@ -1,27 +1,27 @@
 <script setup>
-import { defineLayout } from '@inertiajs/vue3'
 import Layout from '../Components/Layout.vue'
 
-defineLayout(Layout)
 defineProps(['users'])
 </script>
 
 <template>
-  <h1 class="text-3xl">Users</h1>
-  <table>
-    <thead>
-      <tr>
-        <th class="px-4 py-2">Id</th>
-        <th class="px-4 py-2">Name</th>
-        <th class="px-4 py-2">Email</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="user in users" :key="user.id">
-        <td class="border px-4 py-2">{{ user.id }}</td>
-        <td class="border px-4 py-2">{{ user.name }}</td>
-        <td class="border px-4 py-2">{{ user.email }}</td>
-      </tr>
-    </tbody>
-  </table>
+  <Layout>
+    <h1 class="text-3xl">Users</h1>
+    <table>
+      <thead>
+        <tr>
+          <th class="px-4 py-2">Id</th>
+          <th class="px-4 py-2">Name</th>
+          <th class="px-4 py-2">Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in users" :key="user.id">
+          <td class="border px-4 py-2">{{ user.id }}</td>
+          <td class="border px-4 py-2">{{ user.name }}</td>
+          <td class="border px-4 py-2">{{ user.email }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </Layout>
 </template>
