b7fca2adfbafd18522aadc37674fd9df3634cb67
Jonathan Reinink <jonathan@reinink.ca>
2023-01-04 00:03:09 -0500

    Add Tailwind CSS proper to playgrounds

--- playgrounds/react/resources/views/app.blade.php
+++ playgrounds/react/resources/views/app.blade.php
@@ -3,9 +3,8 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    <script src="https://cdn.tailwindcss.com"></script>
     @viteReactRefresh
-    @vite('resources/js/app.jsx')
+    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
     @inertiaHead
   </head>
   <body>
--- playgrounds/vue2/resources/views/app.blade.php
+++ playgrounds/vue2/resources/views/app.blade.php
@@ -3,8 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    <script src="https://cdn.tailwindcss.com"></script>
-    @vite('resources/js/app.js')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     @inertiaHead
   </head>
   <body>
