d002b10d4bdd6407a2f102e06a4b32133e16cbd0
Jonathan Reinink <jonathan@reinink.ca>
2023-01-05 21:38:15 -0500

    Build out Vue 3 playground

--- playgrounds/vue3/resources/js/Components/Layout.vue
+++ playgrounds/vue3/resources/js/Components/Layout.vue
@@ -1,13 +1,20 @@
 <script setup>
-import { Link } from '@inertiajs/vue3'
+import { Link, usePage } from '@inertiajs/vue3'
+import { computed } from 'vue'
+
+const appName = computed(() => usePage().props.value.appName)
 </script>
 
 <template>
-  <nav class="space-x-6 bg-slate-800 p-6 text-white">
-    <Link href="/">Home</Link>
-    <Link href="/users">Users</Link>
+  <nav class="flex items-center space-x-6 bg-slate-800 px-10 py-6 text-white">
+    <div class="rounded-lg bg-slate-700 px-4 py-1">{{ appName }}</div>
+    <Link href="/" class="hover:underline">Home</Link>
+    <Link href="/users" class="hover:underline">Users</Link>
+    <Link href="/article" class="hover:underline">Article</Link>
+    <Link href="/form" class="hover:underline">Form</Link>
+    <Link href="/logout" method="post" as="button" type="button" class="hover:underline">Logout</Link>
   </nav>
-  <main class="p-6">
+  <main class="px-10 py-8">
     <slot />
   </main>
 </template>
--- playgrounds/vue3/resources/js/Pages/Home.vue
+++ playgrounds/vue3/resources/js/Pages/Home.vue
@@ -3,6 +3,14 @@ import Layout from '../Components/Layout.vue'
 export default { layout: Layout }
 </script>
 
+<script setup>
+import { Head, Link } from '@inertiajs/vue3'
+</script>
+
 <template>
+  <Head title="Home" />
   <h1 class="text-3xl">Home</h1>
+  <p class="mt-6">
+    <Link href="/article#far-down" class="text-blue-700 underline">Link to bottom of article page</Link>
+  </p>
 </template>
