4150c12543e84b873f2390de9075b632bc9859e7
Jonathan Reinink <jonathan@reinink.ca>
2023-01-12 08:28:26 -0500

    Remove unnecessary aliases from playgrounds

--- playgrounds/react/vite.config.js
+++ playgrounds/react/vite.config.js
@@ -1,17 +1,8 @@
 import react from '@vitejs/plugin-react'
 import laravel from 'laravel-vite-plugin'
-import path from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@inertiajs/react/server': path.resolve(__dirname, '../../packages/react/dist/server.esm.js'),
-      '@inertiajs/react': path.resolve(__dirname, '../../packages/react/dist/index.esm.js'),
-      '@inertiajs/core/server': path.resolve(__dirname, '../../packages/core/dist/server.esm.js'),
-      '@inertiajs/core': path.resolve(__dirname, '../../packages/core/dist/index.esm.js'),
-    },
-  },
   plugins: [
     laravel({
       input: ['resources/css/app.css', 'resources/js/app.jsx'],
