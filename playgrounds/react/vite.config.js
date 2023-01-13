10a8c2d388fe16fd50035511f0154bb69625a02c
Jonathan Reinink <jonathan@reinink.ca>
2023-01-12 21:53:04 -0500

    Remove array syntax for Vite SSR config

--- playgrounds/react/vite.config.js
+++ playgrounds/react/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [
     laravel({
       input: ['resources/css/app.css', 'resources/js/app.jsx'],
-      ssr: ['resources/js/ssr.jsx'],
+      ssr: 'resources/js/ssr.jsx',
       refresh: true,
     }),
     react({}),
--- playgrounds/svelte/vite.config.js
+++ playgrounds/svelte/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [
     laravel.default({
       input: ['resources/css/app.css', 'resources/js/app.js'],
-      ssr: ['resources/js/ssr.js'],
+      ssr: 'resources/js/ssr.js',
       refresh: true,
     }),
     svelte({
