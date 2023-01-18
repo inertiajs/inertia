b9bfdc0d3caba77073d77973f0d9e6a8fbb62adf
Jess Archer <jess@jessarcher.com>
2023-01-18 12:54:19 +1000

    Temporarily SSR TypeScript errors

--- playgrounds/vue3/resources/js/ssr.ts
+++ playgrounds/vue3/resources/js/ssr.ts
@@ -1,8 +1,10 @@
 import { createInertiaApp } from '@inertiajs/vue3'
+// @ts-expect-error
 import createServer from '@inertiajs/vue3/server'
 import { renderToString } from '@vue/server-renderer'
 import { createSSRApp, h, type DefineComponent } from 'vue'
 
+// @ts-expect-error
 createServer((page) =>
   createInertiaApp({
     page,
