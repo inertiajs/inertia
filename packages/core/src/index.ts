b7f9606cfc5b304bdf24b3079103683fff9798da
Zdravko Curic <zcuric@gmail.com>
2023-02-09 17:20:09 +0100

    Export axios as httpClient for general use

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -6,5 +6,6 @@ export { default as shouldIntercept } from './shouldIntercept'
 export * from './types'
 export { hrefToUrl, mergeDataIntoQueryString, urlWithoutHash } from './url'
 export { type Router }
+export { default as httpClient } from 'axios'
 
 export const router = new Router()
