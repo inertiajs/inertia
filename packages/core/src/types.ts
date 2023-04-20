76221dbe5143d751d4858f1d1cc2e2ebee3e960c
Mathieu TUDISCO <oss@mathieutu.dev>
2023-04-20 03:02:59 +0200

    Fix `usePage()` reactivity in Vue 3 adapter (#1469)
    * [vue3] Replace page ref to reactive.
    
    * Revert previous commit and simplify.
    
    * Update playground to use proper `usePage` implementation
    
    * Formatting
    
    * Remove unused `resolvedErrors` property
    
    This featured was removed back in 2021, see #693.
    
    * Mark private page properties as `@internal`
    
    ---------
    
    Co-authored-by: Jonathan Reinink <jonathan@reinink.ca>

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -37,10 +37,10 @@ export interface Page<SharedProps extends PageProps = PageProps> {
   url: string
   version: string | null
 
-  // Refactor away
+  /** @internal */
   scrollRegions: Array<{ top: number; left: number }>
+  /** @internal */
   rememberedState: Record<string, unknown>
-  resolvedErrors: Errors
 }
 
 export type PageResolver = (name: string) => Component
