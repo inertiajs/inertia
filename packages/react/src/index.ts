1dbd6a774aa87419ae7bf893d654bb8f1759a292
Long Nguyen <olragon@users.noreply.github.com>
2023-06-22 10:14:04 +0700

    Export `InertiaFormProps`
    This is helpful when defining a custom Form wrapper component that will provide custom methods. E.g. `getError(fieldPath: string)`, `getData(fieldPath: string)`.

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -4,6 +4,6 @@ export const router = Router
 export { default as createInertiaApp } from './createInertiaApp'
 export { default as Head } from './Head'
 export { default as Link, InertiaLinkProps } from './Link'
-export { default as useForm } from './useForm'
+export { default as useForm, InertiaFormProps } from './useForm'
 export { default as usePage } from './usePage'
 export { default as useRemember } from './useRemember'
