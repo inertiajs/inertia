f8557cb820fbb6b594a040c8418380ba65f70af4
Kyle Filz <kfilz@live.ca>
2023-11-22 13:53:51 -0600

    Export Vue's InertiaFormProps

--- packages/vue3/src/useForm.ts
+++ packages/vue3/src/useForm.ts
@@ -5,7 +5,7 @@ import { reactive, watch } from 'vue'
 
 type FormDataType = object
 
-interface InertiaFormProps<TForm extends FormDataType> {
+export interface InertiaFormProps<TForm extends FormDataType> {
   isDirty: boolean
   errors: Partial<Record<keyof TForm, string>>
   hasErrors: boolean
