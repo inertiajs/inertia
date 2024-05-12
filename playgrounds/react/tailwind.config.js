99d97b35a1a39f232513ae08dbfbc43061d0420e
James St-Pierre <jamesst20@gmail.com>
2024-05-11 23:32:09 -0400

    React Playground: Upgrade to Laravel 11

--- playgrounds/react/tailwind.config.js
+++ playgrounds/react/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./resources/**/*.blade.php', './resources/**/*.tsx'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ['./resources/**/*.blade.php', './resources/**/*.tsx'],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
