eb2215e58dfec5fa09c947b293369cfe66891fc3
Derrick Reimer <derrickreimer@gmail.com>
2024-06-24 15:51:31 -0500

    Skip intercepting auxiliary (middle) clicks on links

--- packages/core/src/shouldIntercept.ts
+++ packages/core/src/shouldIntercept.ts
@@ -1,4 +1,4 @@
-export default function shouldIntercept(event: KeyboardEvent): boolean {
+export default function shouldIntercept(event: MouseEvent | KeyboardEvent): boolean {
   const isLink = (event.currentTarget as HTMLElement).tagName.toLowerCase() === 'a'
   return !(
     (event.target && (event?.target as HTMLElement).isContentEditable) ||
@@ -7,6 +7,10 @@ export default function shouldIntercept(event: KeyboardEvent): boolean {
     (isLink && event.altKey) ||
     (isLink && event.ctrlKey) ||
     (isLink && event.metaKey) ||
-    (isLink && event.shiftKey)
+    (isLink && event.shiftKey) ||
+    // The auxiliary button (middle button) was pressed. All major browsers
+    // except Safari trigger the `auxclick` instead of `click`, so this is to
+    // ensure we do not intercept middle clicks in Safari.
+    (isLink && 'button' in event && event.button === 1)
   )
 }
