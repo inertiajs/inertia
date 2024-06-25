6012330dac4c7a1c2c169d20bb53fba5d4fd9e46
Derrick Reimer <derrick@savvycal.com>
2024-06-25 11:26:17 -0500

    Skip intercepting non-left button clicks on links (#1908)
    * Skip intercepting auxiliary (middle) clicks on links
    
    * Skip intercepting all non-left button clicks

--- packages/core/src/shouldIntercept.ts
+++ packages/core/src/shouldIntercept.ts
@@ -1,4 +1,4 @@
-export default function shouldIntercept(event: KeyboardEvent): boolean {
+export default function shouldIntercept(event: MouseEvent | KeyboardEvent): boolean {
   const isLink = (event.currentTarget as HTMLElement).tagName.toLowerCase() === 'a'
   return !(
     (event.target && (event?.target as HTMLElement).isContentEditable) ||
@@ -7,6 +7,7 @@ export default function shouldIntercept(event: KeyboardEvent): boolean {
     (isLink && event.altKey) ||
     (isLink && event.ctrlKey) ||
     (isLink && event.metaKey) ||
-    (isLink && event.shiftKey)
+    (isLink && event.shiftKey) ||
+    (isLink && 'button' in event && event.button !== 0)
   )
 }
