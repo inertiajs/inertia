edfc22c5ffbaa64bd14d998364f7b45fb7eb3fa8
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2024-07-09 14:21:37 -0400

    Create Deferred.vue

---
+++ packages/vue3/src/Deferred.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+defineProps<{
+  data: any
+}>()
+</script>
+
+<template>
+  <slot name="loading" v-if="data === undefined" />
+  <slot v-if="data !== undefined" />
+</template>
