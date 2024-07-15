03c934f15861a8f5b1f6867feadef4ec647fb1f5
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2024-07-15 07:35:08 -0400

    grid layout helpers

---
+++ playgrounds/vue3/resources/js/Components/TestGrid.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="mt-6 grid grid-cols-3 gap-4"><slot /></div>
+</template>
---
+++ playgrounds/vue3/resources/js/Components/TestGridItem.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="rounded border border-gray-300 p-4 text-sm text-gray-500">
+    <div class="mb-2 font-bold" v-if="$slots.title">
+      <slot name="title" />
+    </div>
+
+    <slot />
+  </div>
+</template>
