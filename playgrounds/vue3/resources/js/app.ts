7556dbbc4d2fda7f104242da61e379eae2001a31
Robert Boes <2871897+RobertBoes@users.noreply.github.com>
2024-07-21 02:06:19 +0200

    Use unhead vue instead of the base package

--- playgrounds/vue3/resources/js/app.ts
+++ playgrounds/vue3/resources/js/app.ts
@@ -1,10 +1,6 @@
 import { createInertiaApp } from '@inertiajs/vue3'
 import { createSSRApp, h, type DefineComponent } from 'vue'
-import { useHead } from 'unhead'
-
-useHead({
-    titleTemplate: (title) => `${title} - Vue 3 Playground`,
-})
+import { createHead, useHead } from '@unhead/vue'
 
 createInertiaApp({
   resolve: (name) => {
@@ -12,8 +8,15 @@ createInertiaApp({
     return pages[`./Pages/${name}.vue`] as DefineComponent
   },
   setup({ el, App, props, plugin }) {
+    const head = createHead()
+
+    useHead({
+        titleTemplate: (title) => `${title} - Vue 3 Playground`,
+    })
+
     createSSRApp({ render: () => h(App, props) })
       .use(plugin)
+      .use(head)
       .mount(el)
   },
 })
--- playgrounds/vue3/resources/js/ssr.ts
+++ playgrounds/vue3/resources/js/ssr.ts
@@ -2,13 +2,7 @@ import { createInertiaApp } from '@inertiajs/vue3'
 import createServer from '@inertiajs/vue3/server'
 import { renderToString } from '@vue/server-renderer'
 import { createSSRApp, h, type DefineComponent } from 'vue'
-import { createHead, useHead } from 'unhead'
-
-createHead()
-
-useHead({
-    titleTemplate: (title) => `${title} - Vue 3 Playground`,
-})
+import { createHead, useHead } from '@unhead/vue'
 
 createServer((page) =>
   createInertiaApp({
@@ -19,9 +13,15 @@ createServer((page) =>
       return pages[`./Pages/${name}.vue`] as DefineComponent
     },
     setup({ App, props, plugin }) {
+      const head = createHead()
+
+      useHead({
+        titleTemplate: (title) => `${title} - Vue 3 Playground`,
+      })
+
       return createSSRApp({
         render: () => h(App, props),
-      }).use(plugin)
+      }).use(plugin).use(head)
     },
   }),
 )
