536e02a7c4761533b6500f90667cb48d01923d75
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2024-09-12 15:10:23 -0400

    comitting before removing indexed db

--- packages/core/src/encryption.ts
+++ packages/core/src/encryption.ts
@@ -1,24 +1,25 @@
-const getIv = () => {
-  const ivString = window.sessionStorage.getItem('iv')
+export const encrypt = async (data: any): Promise<any> => {
+  const iv = getIv()
+  const history = await getHistoryFromDb(iv)
+  const key = await getOrCreateKey(iv, history)
+  const encrypted = await encryptData(iv, key, data)
 
-  if (!ivString) {
-    const iv = window.crypto.getRandomValues(new Uint8Array(12))
-    window.sessionStorage.setItem('iv', JSON.stringify(Array.from(iv)))
-    return iv
-  }
-
-  return new Uint8Array(JSON.parse(ivString))
+  return encrypted
 }
 
-const createKey = async () => {
-  return window.crypto.subtle.generateKey(
-    {
-      name: 'AES-GCM',
-      length: 256,
-    },
-    true,
-    ['encrypt', 'decrypt'],
-  )
+export const decrypt = async (data: any): Promise<any> => {
+  const iv = getIv()
+  const history = await getHistoryFromDb(iv)
+
+  if (!history) {
+    return null
+  }
+
+  //   try {
+  return await decryptData(iv, history.key, data)
+  //   } catch (e) {
+  //     return null
+  //   }
 }
 
 const encryptData = async (iv: Uint8Array, key: CryptoKey, data: any) => {
@@ -32,173 +33,134 @@ const encryptData = async (iv: Uint8Array, key: CryptoKey, data: any) => {
   )
 }
 
-export const encrypt = async (data: any): Promise<any> => {
-  const start = Date.now()
+const decryptData = async (iv: Uint8Array, key: CryptoKey, data: any) => {
+  const decrypted = await window.crypto.subtle.decrypt(
+    {
+      name: 'AES-GCM',
+      iv,
+    },
+    key,
+    data,
+  )
 
-  //   data = new Array(100).fill(data)
+  return JSON.parse(new TextDecoder().decode(decrypted))
+}
 
-  const iv = getIv()
+const getIv = () => {
+  const ivString = window.sessionStorage.getItem('iv')
 
+  if (ivString) {
+    return new Uint8Array(JSON.parse(ivString))
+  }
+
+  const iv = window.crypto.getRandomValues(new Uint8Array(12))
+
+  window.sessionStorage.setItem('iv', JSON.stringify(Array.from(iv)))
+
+  return iv
+}
+
+const createKey = async () => {
+  return window.crypto.subtle.generateKey(
+    {
+      name: 'AES-GCM',
+      length: 256,
+    },
+    true,
+    ['encrypt', 'decrypt'],
+  )
+}
+
+const clearAllKeys = async () => {
+  const db = await getDb()
+
+  const transaction = db.transaction(['history'], 'readwrite')
+  const objectStore = transaction.objectStore('history')
+
+  objectStore.clear()
+}
+
+const getDb = async (): Promise<IDBDatabase> => {
   return new Promise((resolve) => {
     const dbRequest = indexedDB.open('InertiaDatabase', 1)
 
     dbRequest.onupgradeneeded = function () {
-      const db = dbRequest.result
-      db.createObjectStore('history', { keyPath: 'id' })
+      dbRequest.result.createObjectStore('history', { keyPath: 'id' })
     }
 
     dbRequest.onsuccess = function () {
-      const db = dbRequest.result
-      const transaction = db.transaction(['history'], 'readwrite')
-      const objectStore = transaction.objectStore('history')
-      const getRequest = objectStore.get(iv)
+      resolve(dbRequest.result)
+    }
 
-      getRequest.onsuccess = function () {
-        const history = getRequest.result
-
-        if (history) {
-          //   console.log('got a key!', history)
-          encryptData(iv, history.key, data).then((encrypted) => {
-            // console.log('encrypted', encrypted)
-            console.log('encrypted in', Date.now() - start)
-            resolve(encrypted)
-          })
-        } else {
-          // delete all keys
-          const transaction = db.transaction(['history'], 'readwrite')
-          const objectStore = transaction.objectStore('history')
-          objectStore.clear()
-
-          console.log('No key found, creating now')
-          createKey().then((key) => {
-            // console.log('created key', key)
-
-            const transaction = db.transaction(['history'], 'readwrite')
-            const objectStore = transaction.objectStore('history')
-            objectStore.add({ id: iv, key })
-
-            encryptData(iv, key, data).then((encrypted) => {
-              //   console.log('encrypted', encrypted)
-              console.log('encrypted in', Date.now() - start)
-              resolve(encrypted)
-            })
-          })
-        }
-      }
-
-      getRequest.onerror = function () {
-        console.log('Error', getRequest.error)
-      }
+    dbRequest.onerror = function (...args) {
+      console.error('got an error!!', args)
+      //
     }
   })
 }
 
-export const decrypt = async (data: any): Promise<any> => {
-  const start = Date.now()
-  const iv = getIv()
+const saveKey = async (iv: Uint8Array, key: CryptoKey) => {
+  const db = await getDb()
+
+  const transaction = db.transaction(['history'], 'readwrite')
+  const objectStore = transaction.objectStore('history')
+
+  objectStore.add({ id: iv.toString(), key })
+}
+
+const getOrCreateKey = async (iv: Uint8Array, history: { key: CryptoKey } | null) => {
+  if (history) {
+    return history.key
+  }
+
+  await clearAllKeys()
+
+  const key = await createKey()
+
+  window.crypto.subtle.exportKey('raw', key).then((keyData) => {
+    // window.sessionStorage.setItem('key', JSON.stringify(Array.from(new Uint8Array(keyData))))
+    const storable = JSON.stringify(Array.from(new Uint8Array(keyData)))
+    console.log('keyData', keyData)
+
+    window.crypto.subtle
+      .importKey(
+        'raw',
+        new Uint8Array(JSON.parse(storable)),
+        {
+          name: 'AES-GCM',
+          length: 256,
+        },
+        true,
+        ['encrypt', 'decrypt'],
+      )
+      .then((importedKey) => {
+        console.log('importedKey', importedKey)
+      })
+  })
+
+  await saveKey(iv, key)
+
+  return key
+}
+
+const getHistoryFromDb = async (
+  iv: Uint8Array,
+): Promise<{
+  key: CryptoKey
+} | null> => {
+  const db = await getDb()
 
   return new Promise((resolve) => {
-    const dbRequest = indexedDB.open('InertiaDatabase', 1)
+    const transaction = db.transaction(['history'], 'readonly')
+    const objectStore = transaction.objectStore('history')
+    const getRequest = objectStore.get(iv.toString())
 
-    dbRequest.onupgradeneeded = function () {
-      const db = dbRequest.result
-      db.createObjectStore('history', { keyPath: 'id' })
+    getRequest.onsuccess = function () {
+      resolve(getRequest.result)
     }
 
-    dbRequest.onsuccess = function () {
-      const db = dbRequest.result
-      const transaction = db.transaction(['history'], 'readwrite')
-      const objectStore = transaction.objectStore('history')
-      const getRequest = objectStore.get(iv)
-
-      getRequest.onsuccess = function () {
-        const history = getRequest.result
-
-        if (history) {
-          //   console.log('got a key!', history)
-          window.crypto.subtle
-            .decrypt(
-              {
-                name: 'AES-GCM',
-                iv,
-              },
-              history.key,
-              data,
-            )
-            .then((decrypted) => {
-              const decryptedText = new TextDecoder().decode(decrypted)
-              console.log('decrypted in', Date.now() - start)
-              resolve(JSON.parse(decryptedText))
-            })
-        } else {
-          //   console.log('No history found')
-        }
-      }
-
-      getRequest.onerror = function () {
-        console.log('Error', getRequest.error)
-      }
+    getRequest.onerror = function (...args) {
+      console.error('got an error...', args)
     }
   })
 }
-
-//   window.crypto.subtle
-//     .generateKey(
-//       {
-//         name: 'AES-GCM',
-//         length: 256,
-//       },
-//       true,
-//       ['encrypt', 'decrypt'],
-//     )
-//     .then((key) => {
-//       const iv = window.crypto.getRandomValues(new Uint8Array(12))
-//       // store in sessionStorage
-//       console.log(iv)
-//       window.sessionStorage.setItem('iv', JSON.stringify(Array.from(iv)))
-//       console.log(window.sessionStorage.getItem('iv'))
-//       // restore iv
-//       const ivString = window.sessionStorage.getItem('iv')
-//       const iv2 = new Uint8Array(JSON.parse(ivString!))
-//       console.log(iv2)
-//       const toEncryptJson = JSON.stringify(new Array(100).fill(page))
-
-//       window.crypto.subtle
-//         .encrypt(
-//           {
-//             name: 'AES-GCM',
-//             iv,
-//           },
-//           key,
-//           new TextEncoder().encode(toEncryptJson),
-//         )
-//         .then((encrypted) => {
-//           // console.log('encrypted', encrypted)
-
-//           return window.crypto.subtle.decrypt(
-//             {
-//               name: 'AES-GCM',
-//               iv,
-//             },
-//             key,
-//             encrypted,
-//           )
-//         })
-//         .then((decrypted) => {
-//           // back to human-readable text
-//           const decryptedText = new TextDecoder().decode(decrypted)
-//           // console.log('decrypted', decryptedText)
-//           console.log('time', Date.now() - start)
-//         })
-//       // console.log(key, key.toString())
-//       // window.sessionStorage.setItem('asdf', key.toString())
-//       // Add data to the database
-//       // var request = indexedDB.open('myDatabase', 1)
-//       // request.onsuccess = function (event) {
-//       //   var db = event.target.result
-//       //   var transaction = db.transaction(['customers'], 'readwrite')
-//       //   var objectStore = transaction.objectStore('customers')
-//       //   objectStore.add({ id: 2, name: 'hhu', email: 'john@example.com' })
-//       // }
-//       // console.log(window.sessionStorage.getItem('asdf'))
-//     })
