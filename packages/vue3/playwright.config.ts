c63ccabb45f1bb8ad45c79dbbf8545afde84e75e
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2024-09-16 14:42:07 -0400

    Update playwright.config.ts

--- packages/vue3/playwright.config.ts
+++ packages/vue3/playwright.config.ts
@@ -72,9 +72,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'node tests/app/server.js',
+    url: 'http://localhost:13715',
+    reuseExistingServer: !process.env.CI,
+  },
 })
