a16b7f88f37f1569ae795f2a3ddf85851dad9ab7
Pedro Borges <oi@pedroborg.es>
2024-09-18 14:27:52 -0300

    Fix props reactivity

--- packages/svelte/src/lib/components/Render.svelte
+++ packages/svelte/src/lib/components/Render.svelte
@@ -2,15 +2,22 @@
   import type { PageProps } from '@inertiajs/core'
   import type { ComponentType } from 'svelte'
 
-  type RenderProps = {
+  export type RenderProps = {
     component: ComponentType
     props?: PageProps
     children?: RenderProps[]
+    key?: number | null
   } | null
 
-  export const h = (component: ComponentType, props?: PageProps, children?: RenderProps[]): RenderProps => {
+  export const h = (
+    component: ComponentType,
+    props?: PageProps,
+    children?: RenderProps[],
+    key: number | null = null,
+  ): RenderProps => {
     return {
       component,
+      key,
       ...(props ? { props } : {}),
       ...(children ? { children } : {}),
     }
@@ -18,19 +25,18 @@
 </script>
 
 <script lang="ts">
-  import store from '../store'
-
   export let component: ComponentType
   export let props: PageProps = {}
   export let children: RenderProps[] = []
+  export let key: number | null = null
 </script>
 
-{#if $store.component}
+{#if component}
   <!--
   Add the `key` only to the last (page) component in the tree.
   This ensures that the page component re-renders when `preserveState` is disabled,
   while the layout components are persisted across page changes. -->
-  {#key children?.length === 0 ? $store.key : null}
+  {#key children?.length === 0 ? key : null}
     {#if children.length > 0}
       <svelte:component this={component} {...props}>
         {#each children as child}
