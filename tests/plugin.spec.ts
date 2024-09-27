72e1cdba46108918d433d6254c00937460ae0e7e
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2024-09-27 10:55:04 -0400

    Update plugin.spec.ts

--- tests/plugin.spec.ts
+++ tests/plugin.spec.ts
@@ -1,39 +1,41 @@
 import { expect, test } from '@playwright/test'
 
-test.describe('plugin', () => {
-  test.describe('$page helper', () => {
-    test('has the helper injected into the Vue component', async ({ page }) => {
-      await page.goto('/')
+if (process.env.PACKAGE === 'vue3') {
+  test.describe('plugin', () => {
+    test.describe('$page helper', () => {
+      test('has the helper injected into the Vue component', async ({ page }) => {
+        await page.goto('/')
 
-      const initialPage = await page.evaluate(() => (window as any).initialPage)
-      const $page = await page.evaluate(() => (window as any)._plugin_global_props.$page)
-      await expect(initialPage).not.toBeNull()
-      await expect($page).toEqual(initialPage)
+        const initialPage = await page.evaluate(() => (window as any).initialPage)
+        const $page = await page.evaluate(() => (window as any)._plugin_global_props.$page)
+        await expect(initialPage).not.toBeNull()
+        await expect($page).toEqual(initialPage)
+      })
+
+      test('misses the helper when not registered', async ({ page }) => {
+        await page.goto('/plugin/without')
+
+        const $page = await page.evaluate(() => (window as any)._plugin_global_props.$page)
+        await expect($page).toBeUndefined()
+      })
     })
 
-    test('misses the helper when not registered', async ({ page }) => {
-      await page.goto('/plugin/without')
+    test.describe('$inertia helper', () => {
+      test('has the helper injected into the Vue component', async ({ page }) => {
+        await page.goto('/')
 
-      const $page = await page.evaluate(() => (window as any)._plugin_global_props.$page)
-      await expect($page).toBeUndefined()
+        const $inertia = await page.evaluate(() => (window as any)._plugin_global_props.$inertia)
+        const Inertia = await page.evaluate(() => (window as any).testing.Inertia)
+        await expect($inertia).not.toBeNull()
+        await expect($inertia).toEqual(Inertia)
+      })
+
+      test('misses the helper when not registered', async ({ page }) => {
+        await page.goto('/plugin/without')
+
+        const $inertia = await page.evaluate(() => (window as any)._plugin_global_props.$inertia)
+        await expect($inertia).toBeUndefined()
+      })
     })
   })
-
-  test.describe('$inertia helper', () => {
-    test('has the helper injected into the Vue component', async ({ page }) => {
-      await page.goto('/')
-
-      const $inertia = await page.evaluate(() => (window as any)._plugin_global_props.$inertia)
-      const Inertia = await page.evaluate(() => (window as any).testing.Inertia)
-      await expect($inertia).not.toBeNull()
-      await expect($inertia).toEqual(Inertia)
-    })
-
-    test('misses the helper when not registered', async ({ page }) => {
-      await page.goto('/plugin/without')
-
-      const $inertia = await page.evaluate(() => (window as any)._plugin_global_props.$inertia)
-      await expect($inertia).toBeUndefined()
-    })
-  })
-})
+}
