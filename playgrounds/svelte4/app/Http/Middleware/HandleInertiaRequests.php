b19464e5be6bc303006e1327de2d778194603c04
Pedro Borges <oi@pedroborg.es>
2024-10-01 11:55:43 -0300

    Update playgrounds

--- playgrounds/svelte4/app/Http/Middleware/HandleInertiaRequests.php
+++ playgrounds/svelte4/app/Http/Middleware/HandleInertiaRequests.php
@@ -19,8 +19,6 @@ class HandleInertiaRequests extends Middleware
      * Determines the current asset version.
      *
      * @see https://inertiajs.com/asset-versioning
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
      */
     public function version(Request $request): ?string
     {
@@ -31,8 +29,6 @@ class HandleInertiaRequests extends Middleware
      * Defines the props that are shared by default.
      *
      * @see https://inertiajs.com/shared-data
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
      */
     public function share(Request $request): array
     {
--- playgrounds/svelte5/app/Http/Middleware/RedirectIfAuthenticated.php
+++ playgrounds/svelte5/app/Http/Middleware/RedirectIfAuthenticated.php
@@ -12,7 +12,6 @@ class RedirectIfAuthenticated
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @param  string|null  ...$guards
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
