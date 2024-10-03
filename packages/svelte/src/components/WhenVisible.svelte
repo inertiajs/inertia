d812de0b7603e626c3826d9fb5e2506f7bf6f8cb
Pedro Borges <oi@pedroborg.es>
2024-10-03 10:38:02 -0300

    Add WhenVisible component

---
+++ packages/svelte/src/components/WhenVisible.svelte
@@ -0,0 +1,90 @@
+<script lang="ts">
+  import {
+    router,
+    type ReloadOptions,
+  } from '@inertiajs/core'
+  import { onDestroy, onMount } from 'svelte'
+
+  export let data: string | string[] = ''
+  export let params: ReloadOptions = {}
+  export let buffer: number = 0
+  export let elementTag: keyof HTMLElementTagNameMap = 'div'
+  export let once: boolean = true
+
+  let loaded = false
+  let fetching = false
+  let el: HTMLElement
+  let observer: IntersectionObserver | null = null
+
+  onMount(() => {
+    if (!el) {
+      return
+    }
+
+    observer = new IntersectionObserver(
+      (entries) => {
+        if (!entries[0].isIntersecting) {
+          return
+        }
+
+        if (once) {
+          observer?.disconnect()
+        }
+
+        if (fetching) {
+          return
+        }
+
+        fetching = true
+
+        const reloadParams = getReloadParams()
+
+        router.reload({
+          ...reloadParams,
+          onStart: (event) => {
+            fetching = true
+            reloadParams.onStart?.(event)
+          },
+          onFinish: (event) => {
+            loaded = true
+            fetching = false
+            reloadParams.onFinish?.(event)
+          },
+        })
+      },
+      {
+        rootMargin: `${buffer}px`,
+      },
+    )
+
+    observer.observe(el)
+  })
+
+  onDestroy(() => {
+    observer?.disconnect()
+  })
+
+  function getReloadParams(): Partial<ReloadOptions> {
+    if (data !== '') {
+      return {
+        only: (Array.isArray(data) ? data : [data]) as string[],
+      }
+    }
+
+    if (!params.data) {
+      throw new Error('You must provide either a `data` or `params` prop.')
+    }
+
+    return params
+  }
+</script>
+
+{#if !once || !loaded}
+  <svelte:element bind:this={el} this={elementTag} />
+{/if}
+
+{#if loaded}
+  <slot />
+{:else if ($$slots.fallback)}
+  <slot name="fallback" />
+{/if}
