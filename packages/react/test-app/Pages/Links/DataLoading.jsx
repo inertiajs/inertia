52662b4fcc9cd0c999b267e5dd96ae6b57f6f7aa
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2024-10-07 14:58:38 -0400

    Create DataLoading.jsx

---
+++ packages/react/test-app/Pages/Links/DataLoading.jsx
@@ -0,0 +1,12 @@
+import { Link } from '@inertiajs/react'
+
+export default () => (
+  <div>
+    <Link href="/sleep" class="get">
+      First
+    </Link>
+    <Link href="/sleep" class="get">
+      Second
+    </Link>
+  </div>
+)
