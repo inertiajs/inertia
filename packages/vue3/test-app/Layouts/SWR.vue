e5714613c4da78cd9d853587f477535f93bc9809
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2024-10-08 11:43:25 -0400

    prefetch tests

--- packages/vue3/test-app/Layouts/SWR.vue
+++ packages/vue3/test-app/Layouts/SWR.vue
@@ -4,14 +4,10 @@ import { Link } from '@inertiajs/vue3'
 
 <template>
   <div>
-    <Link href="/prefetch/swr/2" prefetch cacheFor="1s">1s Expired</Link>
-    <Link href="/prefetch/swr/3" prefetch :cacheFor="1000">1s Expired (Number)</Link>
-    <Link href="/prefetch/swr/4" style="margin: 0 20px" :prefetch="['mount', 'hover']" :cacheFor="['1s', '10s']"
-      >1s Stale, 2s Expired</Link
-    >
-    <Link href="/prefetch/swr/5" :prefetch="['mount', 'hover']" :cacheFor="[1000, 10000]">
-      1s Stale, 2s Expired (Number)
-    </Link>
+    <Link href="/prefetch/swr/2" prefetch cache-for="1s">1s Expired</Link>
+    <Link href="/prefetch/swr/3" prefetch :cache-for="1000">1s Expired (Number)</Link>
+    <Link href="/prefetch/swr/4" style="margin: 0 20px" prefetch :cache-for="['1s', '3s']">1s Stale, 2s Expired</Link>
+    <Link href="/prefetch/swr/5" prefetch :cache-for="[1000, 3000]"> 1s Stale, 2s Expired (Number) </Link>
     <div>
       <slot />
     </div>
