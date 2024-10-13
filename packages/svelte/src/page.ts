65140a29bfa3cd0fb2aea73629f2d266bbe6ee16
Sam Lewis <sam.lewis0602@gmail.com>
2024-10-12 21:30:49 -0400

    Svelte fix when using deferred props

--- packages/svelte/src/page.ts
+++ packages/svelte/src/page.ts
@@ -1,6 +1,16 @@
 import { derived } from 'svelte/store'
 import store from './store'
 
-const page = derived(store, ($store) => $store.page)
+let initialized = false
+const page = derived(store, ($store, set) => {
+  // This ensures that page props and the $page store are updated
+  // in the same render, such as when using deferred props.
+  if (initialized && typeof window !== 'undefined') {
+    window.queueMicrotask(() => set($store.page))
+    return
+  }
+  set($store.page)
+  initialized = true
+})
 
 export default page
