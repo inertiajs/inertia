7ab848e2d365ace55c9c672cdad6b47adb865001
Stefan Buhrmester <buhrmi@gmail.com>
2024-10-14 12:12:52 +0700

    fix remember

--- packages/svelte/src/useRemember.ts
+++ packages/svelte/src/useRemember.ts
@@ -4,7 +4,7 @@ import { writable } from 'svelte/store'
 
 
 export default function useRemember<State>(initialState: State, key?: string) {
-  const { router } = getContext<{router: Router}>('frame')
+  const { router } = getContext<{router: Router}>('inertia')
   const restored = router.restore(key) as State | undefined
   const store = writable(restored !== undefined ? restored : initialState)
   const unsubscribe = store.subscribe((state) => router.remember(state, key))
