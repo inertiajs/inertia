801f2b4b7394af440bba3e4aa12d696a0b6a03c0
Pedro Borges <oi@pedroborg.es>
2024-10-14 17:55:29 -0300

    Fix deferred reactivity

--- packages/svelte/src/components/Deferred.svelte
+++ packages/svelte/src/components/Deferred.svelte
@@ -1,16 +1,29 @@
 <script lang="ts">
   import { page } from '../index'
+  import { onDestroy } from 'svelte'
 
   export let data: string | string[]
 
   const keys = Array.isArray(data) ? data : [data]
+  let loaded = false
+
+  const unsubscribe = page.subscribe(({ props }) => {
+    // Ensures the deferred slot isn't loaded before page props update
+    window.queueMicrotask(() => {
+      loaded = keys.every((key) => typeof props[key] !== 'undefined')
+    })
+  })
+
+  onDestroy(() => {
+    unsubscribe()
+  })
 
   if (!$$slots.fallback) {
     throw new Error('`<Deferred>` requires a `<svelte:fragment slot="fallback">` slot')
   }
 </script>
 
-{#if keys.every((key) => $page.props[key] !== undefined)}
+{#if loaded}
   <slot />
 {:else}
   <slot name="fallback" />
