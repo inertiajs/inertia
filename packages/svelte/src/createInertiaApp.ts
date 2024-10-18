a95409f42ae0138ae7df6cfe31f9b5e11f8444c8
Pedro Borges <pedroborges@users.noreply.github.com>
2024-10-17 22:27:33 -0300

    [2.x] Refactor `createInertiaApp` in Svelte adapter (#2036)
    * Refactor state management
    
    * Move SSR logic to user-land
    
    * Toggle "removeComments" TS option back to true
    
    * Fix type error

--- packages/svelte/src/createInertiaApp.ts
+++ packages/svelte/src/createInertiaApp.ts
@@ -1,25 +1,20 @@
 import { router, setupProgress, type InertiaAppResponse, type Page } from '@inertiajs/core'
 import escape from 'html-escape'
 import type { ComponentType } from 'svelte'
-import { version as SVELTE_VERSION } from 'svelte/package.json'
-import App from './components/App.svelte'
-import store from './store'
-import type { ComponentResolver, ResolvedComponent } from './types'
+import App, { type InertiaAppProps } from './components/App.svelte'
+import type { ComponentResolver } from './types'
 
 type SvelteRenderResult = { html: string; head: string; css?: { code: string } }
-type AppComponent = ComponentType<App> & { render: () => SvelteRenderResult }
+type AppComponent = ComponentType<App> & { render: (props: InertiaAppProps) => SvelteRenderResult }
 
 interface CreateInertiaAppProps {
   id?: string
   resolve: ComponentResolver
-  setup?: (props: {
-    el: HTMLElement
-    App: ComponentType<App>
-    props: {
-      initialPage: Page
-      resolveComponent: ComponentResolver
-    }
-  }) => void | App
+  setup: (props: {
+    el: HTMLElement | null
+    App: AppComponent
+    props: InertiaAppProps
+  }) => void | App | SvelteRenderResult
   progress?:
     | false
     | {
@@ -40,31 +35,24 @@ export default async function createInertiaApp({
 }: CreateInertiaAppProps): InertiaAppResponse {
   const isServer = typeof window === 'undefined'
   const el = isServer ? null : document.getElementById(id)
-  const initialPage: Page = page || JSON.parse(el?.dataset?.page || '{}')
+  const initialPage: Page = page || JSON.parse(el?.dataset.page || '{}')
   const resolveComponent = (name: string) => Promise.resolve(resolve(name))
 
-  await Promise.all([resolveComponent(initialPage.component), router.decryptHistory().catch(() => {})]).then(
-    ([initialComponent]) => {
-      store.set({
-        component: initialComponent,
-        page: initialPage,
-        key: null,
-      })
-    },
-  )
+  const [initialComponent] = await Promise.all([
+    resolveComponent(initialPage.component),
+    router.decryptHistory().catch(() => {}),
+  ])
+
+  const props: InertiaAppProps = { initialPage, initialComponent, resolveComponent }
+
+  const svelteApp = setup({
+    el,
+    App: App as unknown as AppComponent,
+    props
+  })
 
   if (isServer) {
-    const isSvelte5 = SVELTE_VERSION.startsWith('5')
-    const { html, head, css } = await (async () => {
-      if (isSvelte5) {
-        const { render } = await dynamicImport('svelte/server')
-        if (typeof render === 'function') {
-          return render(App) as SvelteRenderResult
-        }
-      }
-
-      return (App as AppComponent).render()
-    })()
+    const { html, head, css } = svelteApp as SvelteRenderResult
 
     return {
       body: `<div data-server-rendered="true" id="${id}" data-page="${escape(JSON.stringify(initialPage))}">${html}</div>`,
@@ -72,43 +60,7 @@ export default async function createInertiaApp({
     }
   }
 
-  if (!el) {
-    throw new Error(`Element with ID "${id}" not found.`)
-  }
-
-  router.init({
-    initialPage,
-    resolveComponent,
-    swapComponent: async ({ component, page, preserveState }) => {
-      store.update((current) => ({
-        component: component as ResolvedComponent,
-        page,
-        key: preserveState ? current.key : Date.now(),
-      }))
-    },
-  })
-
   if (progress) {
     setupProgress(progress)
-  }
-
-  setup({
-    el,
-    App,
-    props: {
-      initialPage,
-      resolveComponent,
-    },
-  })
-}
-
-// Loads the module dynamically during execution instead of at
-// build time. The `@vite-ignore` flag prevents Vite from
-// analyzing or pre-bundling this import.
-async function dynamicImport(module: string) {
-  try {
-    return await import(/* @vite-ignore */ module)
-  } catch {
-    return null
   }
 }
