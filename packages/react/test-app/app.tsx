d093a5f40315de3454457221324df3f790087a72
Alexander Dischberg <noxasch@gmail.com>
2025-03-13 15:55:20 +0800

    refactor: convert react test-app to tsx

--- packages/react/test-app/app.tsx
+++ packages/react/test-app/app.tsx
@@ -4,11 +4,8 @@ import { createRoot } from 'react-dom/client'
 createInertiaApp({
   page: window.initialPage,
   resolve: (name) => {
-    if (name === 'FormHelper/Data') {
-      return pages[`./Pages/${name}.tsx`]
-    }
-    
     const pages = import.meta.glob('./Pages/**/*.tsx', { eager: true })
+
     return pages[`./Pages/${name}.tsx`]
   },
   setup({ el, App, props }) {
