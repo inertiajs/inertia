b48fe234cff8da87e4fabb69cc70d43768d788e1
Pascal Baljet <pascal@pascalbaljet.nl>
2025-06-17 12:35:48 +0200

    Refactor

--- packages/react/src/App.ts
+++ packages/react/src/App.ts
@@ -4,6 +4,7 @@ import HeadContext from './HeadContext'
 import PageContext from './PageContext'
 
 let isRouterInitialized = false
+let currentIsInitialPage = true
 
 export default function App({
   children,
@@ -13,8 +14,6 @@ export default function App({
   titleCallback,
   onHeadUpdate,
 }) {
-  let currentIsInitialPage = true
-
   const [current, setCurrent] = useState({
     component: initialComponent || null,
     page: initialPage,
@@ -40,16 +39,15 @@ export default function App({
 
   useEffect(() => {
     router.setSwapComponent(async ({ component, page, preserveState }) => {
-      if (currentIsInitialPage) {
-        currentIsInitialPage = false
-        // return
-      }
-
-      setCurrent((current) => ({
-        component,
-        page,
-        key: preserveState ? current.key : Date.now(),
-      }))
+      setCurrent((current) =>
+        currentIsInitialPage
+          ? current
+          : {
+              component,
+              page,
+              key: preserveState ? current.key : Date.now(),
+            },
+      )
     })
 
     router.on('navigate', () => headManager.forceUpdate())
