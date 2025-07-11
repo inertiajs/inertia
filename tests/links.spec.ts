f9addfda9330e249b1433cd5da5e2fc4532c096f
Pascal Baljet <pascal@pascalbaljet.nl>
2025-07-11 11:38:39 +0200

    Cancel sync request on popstate event (#2429)

--- tests/links.spec.ts
+++ tests/links.spec.ts
@@ -837,6 +837,22 @@ test.describe('data-loading attribute', () => {
   })
 })
 
+test('cancels pending request when navigating back', async ({ page }) => {
+  await page.goto('/links/cancel-sync-request/1')
+  await page.getByRole('link', { name: 'Go to Page 2' }).click()
+  await expect(page).toHaveURL('/links/cancel-sync-request/2')
+  await page.getByRole('link', { name: 'Go to Page 3' }).click() // This one is slow (500ms)
+  await page.goBack()
+  await expect(page).toHaveURL('/links/cancel-sync-request/1')
+  await page.waitForTimeout(750)
+  await expect(page).toHaveURL('/links/cancel-sync-request/1')
+  // make sure page 2 is still in the history stack
+  await page.goForward()
+  await expect(page).toHaveURL('/links/cancel-sync-request/2')
+  await page.goBack()
+  await expect(page).toHaveURL('/links/cancel-sync-request/1')
+})
+
 test('will update href if prop is updated', async ({ page }) => {
   await page.goto('/links/prop-update')
   const link = await page.getByRole('link', { name: 'The Link' })
