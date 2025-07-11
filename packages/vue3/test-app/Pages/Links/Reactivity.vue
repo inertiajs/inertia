d49b598b2fb097e9aefc206411a0cb8d9ef7c115
Pascal Baljet <pascal@pascalbaljet.nl>
2025-07-11 18:02:37 +0200

    WIP

---
+++ packages/vue3/test-app/Pages/Links/Reactivity.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { Method } from '@inertiajs/core'
+import { Link } from '@inertiajs/vue3'
+import { ref } from 'vue'
+
+const method = ref<Method>('get')
+const href = ref('/dump/get')
+const data = ref({ foo: 'bar' })
+const headers = ref({ 'X-Custom-Header': 'value' })
+
+const change = () => {
+  method.value = 'post'
+  href.value = '/dump/post'
+  data.value = { foo: 'baz' }
+  headers.value = { 'X-Custom-Header': 'new-value' }
+}
+</script>
+
+<template>
+  <div>
+    <span class="text">
+      This page demonstrates reactivity in Inertia links. Click the button to change the link properties.
+    </span>
+
+    <Link :method="method" :href="href" :data="data" :headers="headers">Submit</Link>
+    <button @click="change">Change Link props</button>
+  </div>
+</template>
