dd963830f0cd146bcd22dea197faa9561766720a
Pascal Baljet <pascal@pascalbaljet.nl>
2025-08-01 00:20:41 +0200

    Update typescript.spec.ts

--- tests/typescript.spec.ts
+++ tests/typescript.spec.ts
@@ -1,12 +1,10 @@
 import test from '@playwright/test'
-import reactUseForm from '../packages/react/src/useForm'
-import vueUseForm from '../packages/vue3/src/useForm'
+import useForm from '../packages/vue3/src/useForm'
 
-// TODO: Unify React/Vue tests while keeping TS safety
+// TODO: Move to real test page/route
 test.describe('TypeScript Support', () => {
   // @ts-expect-error
-  const adapter = process.env.PACKAGE || 'vue3'
-  test.skip(adapter === 'svelte', 'Skipping Svelte for now')
+  test.skip(process.env.PACKAGE !== 'vue3', 'Only implemented for Vue 3')
 
   test.describe('Form Helper', () => {
     test('basic form data', async () => {
@@ -20,25 +18,13 @@ test.describe('TypeScript Support', () => {
         users: [],
       }
 
-      if (adapter === 'react') {
-        const form = reactUseForm<FormData>(defaultData)
-        form.setData('name', 'John Doe')
-        form.setData('users', [{ name: 'Jane Doe' }])
-        // @ts-expect-error - A form has no email field
-        form.setData('email', 'john@example.com')
-        // @ts-expect-error - A form has no email field
-        form.setData('users', [{ name: 'Jane Doe', email: 'jane@example.com' }])
-      }
-
-      if (adapter === 'vue3') {
-        const form = vueUseForm<FormData>(defaultData)
-        form.name = 'John Doe'
-        form.users = [{ name: 'Jane Doe' }]
-        // @ts-expect-error - A form has no email field
-        form.email = 'john@example.com'
-        // @ts-expect-error - A form has no email field
-        form.users = [{ name: 'Jane Doe', email: 'jane@example.com' }]
-      }
+      const form = useForm<FormData>(defaultData)
+      form.name = 'John Doe'
+      form.users = [{ name: 'Jane Doe' }]
+      // @ts-expect-error - A form has no email field
+      form.email = 'john@example.com'
+      // @ts-expect-error - A form has no email field
+      form.users = [{ name: 'Jane Doe', email: 'jane@example.com' }]
     })
 
     test('nested error access', async () => {
@@ -52,20 +38,10 @@ test.describe('TypeScript Support', () => {
         users: [],
       }
 
-      if (adapter === 'react') {
-        const form = reactUseForm<FormData>(defaultData)
-
-        form.clearErrors('users.0.name')
-        form.resetAndClearErrors('users.0.name')
-        form.setError('users.0.name', 'User name is required')
-      }
-
-      if (adapter === 'vue3') {
-        const form = vueUseForm<FormData>(defaultData)
-        form.clearErrors('users.0.name')
-        form.resetAndClearErrors('users.0.name')
-        form.setError('users.0.name', 'User name is required')
-      }
+      const form = useForm<FormData>(defaultData)
+      form.clearErrors('users.0.name')
+      form.resetAndClearErrors('users.0.name')
+      form.setError('users.0.name', 'User name is required')
     })
   })
 })
