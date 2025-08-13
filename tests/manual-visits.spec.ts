40e38ace181316670493fd3354e70796fb3f59c5
pintend <67613598+pintend@users.noreply.github.com>
2025-08-13 05:45:33 -0400

    Fix core: Queue processing when an item fails (#2467)
    * always clear processingPromise when the promise is done
    
    * Added test
    
    ---------
    
    Co-authored-by: Pascal Baljet <pascal@pascalbaljet.nl>

--- tests/manual-visits.spec.ts
+++ tests/manual-visits.spec.ts
@@ -926,3 +926,28 @@ test.describe('Redirects', () => {
     await expect(pageLoads.count).toBe(2)
   })
 })
+
+test('can do a subsequent visit after the previous visit has thrown an error in onSuccess', async ({ page }) => {
+  pageLoads.watch(page)
+  const consoleErrors = []
+
+  page.on('pageerror', (error: Error) => consoleErrors.push(error.message))
+
+  await expect(consoleMessages.messages).toHaveLength(0)
+
+  await page.goto('/visits/after-error/1')
+  const response = page.waitForResponse('/visits/after-error/2')
+
+  await page.getByRole('link', { name: 'Throw error on success' }).click()
+  await response
+
+  await expect(consoleErrors).toHaveLength(1)
+  await expect(consoleErrors[0]).toBe('Error after visit')
+
+  await page.getByRole('link', { name: 'Visit dump page' }).click()
+
+  const dump = await shouldBeDumpPage(page, 'get')
+
+  await expect(dump.method).toBe('get')
+  await expect(dump.form).toEqual({})
+})
