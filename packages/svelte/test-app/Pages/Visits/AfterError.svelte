701c09dc5e555ef762ed5e1d5e72ef1cfb3bfcc5
Pascal Baljet <pascal@pascalbaljet.nl>
2025-08-13 11:42:09 +0200

    Added test

---
+++ packages/svelte/test-app/Pages/Visits/AfterError.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { router } from '@inertiajs/svelte'
+
+  const visitDump = (e) => {
+    e.preventDefault()
+    router.visit('/dump/get')
+  }
+
+  const throwErrorOnSuccess = (e) => {
+    e.preventDefault()
+
+    router.visit('/visits/after-error/2', {
+      onSuccess: () => {
+        throw new Error('Error after visit')
+      }
+    })
+  }
+</script>
+
+<div>
+  <a href="#" on:click={visitDump}>
+    Visit dump page
+  </a>
+
+  <a href="#" on:click={throwErrorOnSuccess}>
+    Throw error on success
+  </a>
+</div>
---
+++ packages/vue3/test-app/Pages/Visits/AfterError.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <a href="#" @click="visitDump">
+      Visit dump page
+    </a>
+
+    <a href="#" @click="throwErrorOnSuccess">
+      Throw error on success
+    </a>
+  </div>
+</template>
+
+<script setup>
+import { router } from '@inertiajs/vue3'
+
+const visitDump = (e) => {
+  e.preventDefault()
+  router.visit('/dump/get')
+}
+
+const throwErrorOnSuccess = (e) => {
+  e.preventDefault()
+
+  router.visit('/visits/after-error/2', {
+    onSuccess: () => {
+      throw new Error('Error after visit')
+    }
+  })
+}
+</script>
