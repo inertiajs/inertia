aae2ba34445a8386c1faaea15302c9f603d4d4b2
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2025-08-15 14:07:03 -0400

    vue resetOnError, resetOnSuccess, setDefaultsOnSuccess

---
+++ packages/vue3/test-app/Pages/FormComponent/ResetAttributes/ResetOnSuccess.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { Form } from '@inertiajs/vue3'
+</script>
+
+<template>
+  <Form method="post" action="/form-component/reset-on-success" reset-on-success>
+    <div>
+      <label for="name">Name</label>
+      <input type="text" name="name" id="name" value="John Doe" />
+      <p id="error_name">{{ $page.props.errors?.name }}</p>
+    </div>
+
+    <div>
+      <label for="email">Email</label>
+      <input type="email" name="email" id="email" value="john@doe.biz" />
+      <p id="error_email">{{ $page.props.errors?.email }}</p>
+    </div>
+
+    <button type="submit">Submit</button>
+  </Form>
+</template>
