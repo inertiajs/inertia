23dea1a35dce612498e155aa4c517a4734b0dc3e
Pascal Baljet <pascal@pascalbaljet.nl>
2025-08-19 13:27:39 +0200

    Fix for nullable object types in `useForm`

---
+++ packages/svelte/test-app/Pages/FormHelper/TypeScript/Nullable.svelte
@@ -0,0 +1,12 @@
+<script  lang="ts">
+// This component is used for checking the TypeScript implementation; there is no Playwright test depending on it.
+import { useForm } from '@inertiajs/svelte'
+
+useForm<{ object: { x: number } | null }>({
+  object: null,
+})
+
+useForm<{ object: { x: number } | null }>({
+  object: { x: 1 },
+})
+</script>
