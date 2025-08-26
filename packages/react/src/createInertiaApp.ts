388f50c80a20b11bafd154383b122fb79eb941e7
HichemTab-tech <konanhichemsinshi@gmail.com>
2025-08-26 15:44:16 +0100

    replace ts-expect-error with ts-ignore

--- packages/react/src/createInertiaApp.ts
+++ packages/react/src/createInertiaApp.ts
@@ -77,7 +77,7 @@ export default async function createInertiaApp<SharedProps extends PageProps = P
   const isServer = typeof window === 'undefined'
   const el = isServer ? null : document.getElementById(id)
   const initialPage = page || JSON.parse(el.dataset.page)
-  // @ts-expect-error
+  // @ts-ignore
   const resolveComponent = (name, page) => Promise.resolve(resolve(name, page)).then((module) => module.default || module)
 
   let head = []
