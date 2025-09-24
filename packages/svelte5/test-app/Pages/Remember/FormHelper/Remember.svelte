bc59e377552c85a09b2b613422ec3fb75414f31b
Brodie <32996052+brodienguyen@users.noreply.github.com>
2025-09-24 12:24:46 +1000

    refactor(remember): remove $ prefix from form references
    Update all references from $form to form in Remember.svelte to standardize variable usage and improve code clarity.

--- packages/svelte5/test-app/Pages/Remember/FormHelper/Remember.svelte
+++ packages/svelte5/test-app/Pages/Remember/FormHelper/Remember.svelte
@@ -10,35 +10,35 @@
   let untracked = ''
 
   const submit = () => {
-    $form.post('/remember/form-helper/remember')
+    form.post('/remember/form-helper/remember')
   }
 
   const reset = () => {
-    $form.reset('handle').clearErrors('name')
+    form.reset('handle').clearErrors('name')
   }
 </script>
 
 <div>
   <label>
     Full Name
-    <input type="text" id="name" name="name" bind:value={$form.name} />
+    <input type="text" id="name" name="name" bind:value={form.name} />
   </label>
-  {#if $form.errors.name}
-    <span class="name_error">{$form.errors.name}</span>
+  {#if form.errors.name}
+    <span class="name_error">{form.errors.name}</span>
   {/if}
   <label>
     Handle
-    <input type="text" id="handle" name="handle" bind:value={$form.handle} />
+    <input type="text" id="handle" name="handle" bind:value={form.handle} />
   </label>
-  {#if $form.errors.handle}
-    <span class="handle_error">{$form.errors.handle}</span>
+  {#if form.errors.handle}
+    <span class="handle_error">{form.errors.handle}</span>
   {/if}
   <label>
     Remember Me
-    <input type="checkbox" id="remember" name="remember" bind:checked={$form.remember} />
+    <input type="checkbox" id="remember" name="remember" bind:checked={form.remember} />
   </label>
-  {#if $form.errors.remember}
-    <span class="remember_error">{$form.errors.remember}</span>
+  {#if form.errors.remember}
+    <span class="remember_error">{form.errors.remember}</span>
   {/if}
   <label>
     Untracked
