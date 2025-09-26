c2b5c8b98800d8c7ee0ce204a77d4a869dcf6ce5
Pascal Baljet <pascal@pascalbaljet.nl>
2025-09-26 10:24:54 +0200

    Update types.ts

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -105,6 +105,13 @@ export interface PageProps {
   [key: string]: unknown
 }
 
+export type ScrollProp = {
+  pageName: string
+  previousPage?: number | string
+  nextPage?: number | string
+  currentPage?: number | string
+}
+
 export interface Page<SharedProps extends PageProps = PageProps> {
   component: string
   props: PageProps &
@@ -121,6 +128,7 @@ export interface Page<SharedProps extends PageProps = PageProps> {
   prependProps?: string[]
   deepMergeProps?: string[]
   matchPropsOn?: string[]
+  scrollProps?: Record<keyof PageProps, ScrollProp>
 
   /** @internal */
   rememberedState: Record<string, unknown>
