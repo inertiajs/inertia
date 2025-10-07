b5ffb461906e8b6322be2e24483deb2cfd2fce01
Pascal Baljet <pascal@pascalbaljet.nl>
2025-10-07 12:56:12 +0200

    Svelte

---
+++ packages/svelte/test-app/Pages/Users/Edit.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { useForm } from '@inertiajs/svelte'
+
+  interface User {
+    id: number
+    name: string
+    email: string
+  }
+
+  export let user: User
+  export let withRememberKey: boolean = false
+
+  const form = withRememberKey
+    ? useForm('EditUserForm', {
+        name: user.name,
+        email: user.email,
+      })
+    : useForm({
+        name: user.name,
+        email: user.email,
+      })
+</script>
+
+<div>
+  <h1>Edit User {user.id}</h1>
+  <p>With remember key: {withRememberKey}</p>
+  <form>
+    <div>
+      <label>Name:</label>
+      <input type="text" bind:value={$form.name} />
+    </div>
+    <div>
+      <label>Email:</label>
+      <input type="email" bind:value={$form.email} />
+    </div>
+  </form>
+</div>
