98fbe0f8d06f6e55329becbd102ea3d61fab68e8
Pascal Baljet <pascal@pascalbaljet.nl>
2025-10-07 09:24:10 +0200

    React + Svelte

--- packages/react/test-app/Pages/InfiniteScroll/UpdateQueryString.tsx
+++ packages/react/test-app/Pages/InfiniteScroll/UpdateQueryString.tsx
@@ -1,7 +1,16 @@
-import { InfiniteScroll } from '@inertiajs/react'
+import { InfiniteScroll, usePage } from '@inertiajs/react'
 import UserCard, { User } from './UserCard'
 
 export default ({ users }: { users: { data: User[] } }) => {
+  const page = usePage()
+
+  window.testing = {
+    ...(window.testing || {}),
+    get pageUrl() {
+      return page.url
+    },
+  }
+
   return (
     <InfiniteScroll
       data="users"
