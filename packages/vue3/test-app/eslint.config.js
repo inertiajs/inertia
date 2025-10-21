eaa4f8b88faaf35dda18f5a3d912312326f2fa2a
Pascal Baljet <pascal@pascalbaljet.nl>
2025-10-21 14:25:30 +0200

    Update eslint.config.js

--- packages/vue3/test-app/eslint.config.js
+++ packages/vue3/test-app/eslint.config.js
@@ -7,15 +7,7 @@ export default defineConfigWithVueTs(
   vue.configs['flat/essential'],
   vueTsConfigs.recommended,
   {
-    ignores: [
-      'vendor',
-      'node_modules',
-      'public',
-      'bootstrap/ssr',
-      'tailwind.config.js',
-      'resources/js/components/ui/*',
-      'dist/**',
-    ],
+    ignores: ['node_modules', 'dist/**/*'],
   },
   {
     rules: {
