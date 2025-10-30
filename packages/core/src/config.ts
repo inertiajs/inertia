e489604eaaf212e490a35920bdebfa299e7b4771
Pascal Baljet <pascal@pascalbaljet.nl>
2025-10-30 18:01:11 +0100

    Improve types for config

--- packages/core/src/config.ts
+++ packages/core/src/config.ts
@@ -28,8 +28,12 @@ type ConfigSetObject<T> = {
   [K in ConfigKeys<T>]?: ConfigValue<T, K>
 }
 
+type FirstLevelOptional<T> = {
+  [K in keyof T]?: T[K] extends object ? { [P in keyof T[K]]?: T[K][P] } : T[K]
+}
+
 export class Config<TConfig extends {} = {}> {
-  protected config: Partial<TConfig> = {}
+  protected config: FirstLevelOptional<TConfig> = {}
   protected defaults: TConfig
 
   public constructor(defaults: TConfig) {
@@ -44,7 +48,7 @@ export class Config<TConfig extends {} = {}> {
     return this as unknown as Config<TConfig & TExtension>
   }
 
-  public replace(newConfig: Partial<TConfig>): void {
+  public replace(newConfig: FirstLevelOptional<TConfig>): void {
     this.config = newConfig
   }
 
@@ -79,3 +83,5 @@ export const config = new Config<InertiaAppConfig>({
     cacheFor: 30_000,
   },
 })
+
+// const _delay = config.get('prefetch.hoverDelay')
