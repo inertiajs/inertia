7309b458e3c00b0bdb0e22686d500b41ddfb84f1
puRe1337 <ds@dxme.dev>
2025-10-31 17:21:22 +0100

    refactor: types in Deferred component & fix side-effects

--- packages/svelte5/src/components/Deferred.svelte
+++ packages/svelte5/src/components/Deferred.svelte
@@ -1,31 +1,31 @@
 <script lang="ts">
-  import { pageState } from '../page.svelte'
+  import { untrack } from 'svelte'
+  import { page } from '../index'
 
-  const {
-    data,
-    children,
-    fallback,
-  }: {
-    data: string | string[]
-    children?: any
-    fallback?: any
-  } = $props()
+  interface Props {
+    data: string | string[];
+    fallback?: import('svelte').Snippet;
+    children?: import('svelte').Snippet;
+  }
+
+  let { data, fallback, children }: Props = $props();
 
   const keys = Array.isArray(data) ? data : [data]
   let loaded = $state(false)
 
   const isServer = typeof window === 'undefined'
-
   if (!isServer) {
     // Use $effect to watch for changes in pageState.props
     $effect(() => {
       // Access pageState.props to make this effect reactive
-      const props = pageState.props
+      const props = page.props
 
-      // Ensures the content isn't loaded before the deferred props are available
-      window.queueMicrotask(() => {
-        const newLoaded = keys.every((key) => typeof props[key] !== 'undefined')
-        loaded = newLoaded
+      // Wrap this up into untrack, to make sure it doesn't gets picked as a depedency to retrigger the $effect
+      untrack(() => {
+        // Ensures the content isn't loaded before the deferred props are available
+        window.queueMicrotask(() => {
+          loaded = keys.every((key) => typeof props[key] !== 'undefined')
+        })
       })
     })
   }
