674182ead5d244c24672ad1455466fff4991def1
HichemTab-tech <konanhichemsinshi@gmail.com>
2025-11-04 00:21:22 +0100

    refactor: replace useContext with use for PageContext

--- packages/react/src/usePage.ts
+++ packages/react/src/usePage.ts
@@ -1,9 +1,9 @@
 import { Page, PageProps, SharedPageProps } from '@inertiajs/core'
-import { useContext } from 'react'
+import { use } from 'react'
 import PageContext from './PageContext'
 
 export default function usePage<TPageProps extends PageProps = PageProps>(): Page<TPageProps & SharedPageProps> {
-  const page = useContext(PageContext)
+  const page = use(PageContext)
 
   if (!page) {
     throw new Error('usePage must be used within the Inertia component')
