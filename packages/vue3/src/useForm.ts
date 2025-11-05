ac38be747c4d7d0e4e68ce9e5b0c6982d66b5310
Pascal Baljet <pascal@pascalbaljet.nl>
2025-11-05 18:00:14 +0100

    Update useForm.ts

--- packages/vue3/src/useForm.ts
+++ packages/vue3/src/useForm.ts
@@ -441,14 +441,17 @@ export default function useForm<TForm extends FormDataType<TForm>>(
   watch(
     form as any as InertiaForm<TForm> & RememberInternalState<TForm>,
     (newValue) => {
-      const formValue = newValue
-      formValue.isDirty = !isEqual(formValue.data(), defaults)
+      newValue.isDirty = !isEqual(newValue.data(), defaults)
       if (rememberKey) {
-        router.remember(cloneDeep(formValue.__remember()), rememberKey)
+        router.remember(cloneDeep(newValue.__remember()), rememberKey)
       }
     },
     { immediate: true, deep: true },
   )
+
+  if (!precognitionEndpoint) {
+    return form
+  }
 
   // Vue's reactive() wrapper matches our interface at runtime
   const typedForm = form as any as InertiaForm<TForm>
