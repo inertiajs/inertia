724cc6817cfabe92628e2d154ce88ea750b62cbc
Svyatoslav Kryukov <me@skryukov.dev>
2025-11-10 23:58:07 +0300

    Export page component type for React adapter

--- playgrounds/react/resources/js/app.tsx
+++ playgrounds/react/resources/js/app.tsx
@@ -1,10 +1,10 @@
-import { createInertiaApp } from '@inertiajs/react'
+import { type ResolvedComponent, createInertiaApp } from '@inertiajs/react'
 import { hydrateRoot } from 'react-dom/client'
 
 createInertiaApp({
   title: (title) => `${title} - React Playground`,
   resolve: (name) => {
-    const pages = import.meta.glob('./Pages/**/*.tsx', { eager: true })
+    const pages = import.meta.glob<ResolvedComponent>('./Pages/**/*.tsx', { eager: true })
     return pages[`./Pages/${name}.tsx`]
   },
   setup({ el, App, props }) {
