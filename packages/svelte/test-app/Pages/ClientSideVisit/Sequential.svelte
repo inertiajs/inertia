46436daee8180c7c39500a18864c773e5c822590
Pascal Baljet <pascal@pascalbaljet.nl>
2025-12-09 11:51:43 +0100

    Fix for sequential client side visits (#2737)

---
+++ packages/svelte/test-app/Pages/ClientSideVisit/Sequential.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { router } from '@inertiajs/svelte'
+
+  export let foo = ''
+  export let bar = ''
+
+  const replaceSequentially = () => {
+    router.replaceProp('foo', 'baz')
+    router.replaceProp('bar', 'qux')
+  }
+</script>
+
+<div>
+  <p>Foo: {foo}</p>
+  <p>Bar: {bar}</p>
+
+  <button on:click={replaceSequentially}>Replace foo and bar sequentially</button>
+</div>
