d3990f3521dda02195a0445493fe75222621d2c5
Pascal Baljet <pascal@pascalbaljet.nl>
2026-01-05 17:25:31 +0100

    test

--- packages/vue3/test-app/Pages/Poll/UnchangedData.vue
+++ packages/vue3/test-app/Pages/Poll/UnchangedData.vue
@@ -1,11 +1,31 @@
 <script setup lang="ts">
 import { usePoll } from '@inertiajs/vue3'
+import { onMounted, ref } from 'vue'
+
+const replaceStateCalls = ref(0)
+const pollsFinished = ref(0)
+
+onMounted(() => {
+  const original = window.history.replaceState.bind(window.history)
+  window.history.replaceState = function (...args) {
+    replaceStateCalls.value++
+    return original(...args)
+  }
+})
 
 usePoll(500, {
   only: ['custom_prop'],
+  onFinish: () => pollsFinished.value++,
 })
 </script>
 
 <template>
-  <div />
+  <div>
+    <p>
+      replaceState calls: <span class="replaceStateCalls">{{ replaceStateCalls }}</span>
+    </p>
+    <p>
+      polls finished: <span class="pollsFinished">{{ pollsFinished }}</span>
+    </p>
+  </div>
 </template>
