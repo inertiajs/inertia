4060cd7e65b657bc2f7223a1ecc0a501822f4408
Pascal Baljet <pascal@pascalbaljet.nl>
2026-01-20 16:30:30 +0100

    Pass `true` to `inert` attribute in React 19 (#2831)

--- packages/react/src/usePage.ts
+++ packages/react/src/usePage.ts
@@ -1,10 +1,11 @@
 import { Page, PageProps, SharedPageProps } from '@inertiajs/core'
 import React from 'react'
 import PageContext from './PageContext'
+import { isReact19 } from './react'
 
 export default function usePage<TPageProps extends PageProps = PageProps>(): Page<TPageProps & SharedPageProps> {
   // React.use() was introduced in React 19, fallback to React.useContext() for earlier versions
-  const page = typeof React.use === 'function' ? React.use(PageContext) : React.useContext(PageContext)
+  const page = isReact19 ? React.use(PageContext) : React.useContext(PageContext)
 
   if (!page) {
     throw new Error('usePage must be used within the Inertia component')
