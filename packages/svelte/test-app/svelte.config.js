5bb157be808ee2472a2835cdf2df75f5b31cabcf
Pascal Baljet <pascal@pascalbaljet.nl>
2026-01-20 11:57:43 +0100

    wip

--- packages/svelte/test-app/svelte.config.js
+++ packages/svelte/test-app/svelte.config.js
@@ -1,12 +1,6 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 const config = {
-  onwarn(warning, onwarn) {
-    if (/A11y/.test(warning.message)) return
-
-    onwarn(warning)
-  },
-
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
   preprocess: vitePreprocess(),
