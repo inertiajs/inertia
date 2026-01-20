14b2f3cf565218b0523e201801d908f1424e5a83
Pascal Baljet <pascal@pascalbaljet.nl>
2026-01-20 10:22:56 +0100

    Add test for Precognition validation with transform key changes (#2827)
    * Precognition test with transform key changes
    
    * Fix code style
    
    * Fix TS
    
    * ts

---
+++ packages/vue3/test-app/Pages/FormComponent/Precognition/TransformKeys.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { Form } from '@inertiajs/vue3'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const transformData = (data: Record<string, any>) => {
+  const document = data.document || {}
+  return document
+}
+</script>
+
+<template>
+  <div>
+    <h1>Form Precognition Transform Keys</h1>
+
+    <Form action="/precognition/transform-keys" method="post" :validationTimeout="100" :transform="transformData">
+      <template #default="{ invalid, errors, validate, valid, validating }">
+        <div>
+          <input
+            id="email-input"
+            name="document[customer][email]"
+            placeholder="Email"
+            @blur="() => validate('customer.email')"
+          />
+          <p v-if="invalid('customer.email')">{{ errors['customer.email'] }}</p>
+          <p v-if="valid('customer.email')">Email is valid!</p>
+        </div>
+
+        <p v-if="validating">Validating...</p>
+      </template>
+    </Form>
+  </div>
+</template>
