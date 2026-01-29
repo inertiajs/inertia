d571dd1f622b44fb7d3b44f595e78ca9129c2e34
Pascal Baljet <pascal@pascalbaljet.nl>
2026-01-29 17:12:13 +0100

    Fix `useForm` type inference in generic wrapper components (#2851)

---
+++ packages/svelte/test-app/Pages/FormHelper/TypeScript/WrapperChild.svelte
@@ -0,0 +1,12 @@
+<!-- eslint-disable @typescript-eslint/no-explicit-any -->
+<script lang="ts" generics="T extends Record<string, any>">
+  import { useForm } from '@inertiajs/svelte'
+
+  export let data: T
+
+  const form = useForm(data)
+</script>
+
+<form>
+  <slot form={$form} />
+</form>
--- packages/vue3/src/useForm.ts
+++ packages/vue3/src/useForm.ts
@@ -126,9 +126,11 @@ export type InertiaPrecognitiveForm<TForm extends object> = InertiaForm<TForm> &
 
 type ReservedFormKeys = keyof InertiaFormProps<any>
 
-type ValidateFormData<T> = {
-  [K in keyof T]: K extends ReservedFormKeys ? ['Error: This field name is reserved by useForm:', K] : T[K]
-}
+type ValidateFormData<T> = string extends keyof T
+  ? T
+  : {
+      [K in keyof T]: K extends ReservedFormKeys ? ['Error: This field name is reserved by useForm:', K] : T[K]
+    }
 
 export default function useForm<TForm extends FormDataType<TForm>>(
   method: Method | (() => Method),
