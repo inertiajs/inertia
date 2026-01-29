3d87a937cb801d754f6ef9a16fa5f4b8d7ec8437
Pascal Baljet <pascal@pascalbaljet.nl>
2026-01-29 10:10:00 +0100

    Fix `useForm` type inference in generic wrapper

---
+++ packages/svelte/test-app/Pages/FormHelper/TypeScript/WrapperChild.svelte
@@ -0,0 +1,11 @@
+<script lang="ts" generics="T extends Record<string, any>">
+  import { useForm } from '@inertiajs/svelte'
+
+  export let data: T
+
+  const form = useForm(data)
+</script>
+
+<form>
+  <slot form={$form} />
+</form>
---
+++ packages/vue3/test-app/Pages/FormHelper/TypeScript/WrapperChild.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts" generic="T extends Record<string, any>">
+import { useForm } from '@inertiajs/vue3'
+
+const props = defineProps<{ data: T }>()
+
+const form = useForm(props.data)
+</script>
+
+<template>
+  <form>
+    <slot :form="form" />
+  </form>
+</template>
