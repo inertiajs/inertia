aa1fe54d1e002881e51b96261e21475064455064
Pascal Baljet <pascal@pascalbaljet.nl>
2026-02-04 15:11:20 +0100

    pre-demo

--- playgrounds/vue3/vite.config.ts
+++ playgrounds/vue3/vite.config.ts
@@ -11,9 +11,10 @@ export default defineConfig({
   plugins: [
     laravel({
       input: ['resources/css/app.css', 'resources/js/app.ts'],
+      ssr: 'resources/js/ssr.ts',
       refresh: true,
     }),
-    inertia({ ssr: { debug: true } }),
+    inertia(),
     vue({
       template: {
         transformAssetUrls: {
