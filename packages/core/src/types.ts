fb1460ed9613ce97dce136a578ad601ffcb0b8c9
🏔️ Peak <122374094+peaklabs-dev@users.noreply.github.com>
2026-03-11 17:01:09 +0100

    [v3.x] fix(types): module augmentation example (#2945)
    * fix(types): module augmentation example
    
    * Update types.ts
    
    ---------
    
    Co-authored-by: Pascal Baljet <pascal@pascalbaljet.nl>

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -24,6 +24,9 @@ export type DefaultInertiaConfig = {
  * @see {@link DefaultInertiaConfig} for keys to override
  * @example
  * ```ts
+ * // global.d.ts
+ * import '@inertiajs/core'
+ *
  * declare module '@inertiajs/core' {
  *   export interface InertiaConfig {
  *     errorValueType: string[]
