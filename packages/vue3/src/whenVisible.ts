41d57d093ba26cba931c87cafe381246ade86ecc
Pascal Baljet <pascal@pascalbaljet.nl>
2026-03-11 22:31:26 +0100

    [3.x] Convert Vue adapter components to Composition API (#2947)
    * Migrate Vue to Composition API
    
    * Update whenVisible.ts

--- packages/vue3/src/whenVisible.ts
+++ packages/vue3/src/whenVisible.ts
@@ -1,6 +1,6 @@
 import { ReloadOptions, router } from '@inertiajs/core'
 import { get } from 'lodash-es'
-import { defineComponent, h, PropType, SlotsType } from 'vue'
+import { computed, defineComponent, h, nextTick, onUnmounted, PropType, ref, SlotsType, watch } from 'vue'
 import { usePage } from './app'
 
 export default defineComponent({
@@ -29,110 +29,110 @@ export default defineComponent({
       default: false,
     },
   },
-  data() {
-    return {
-      loaded: false,
-      fetching: false,
-      observer: null as IntersectionObserver | null,
-    }
-  },
-  unmounted() {
-    this.observer?.disconnect()
-  },
-  computed: {
-    keys(): string[] {
-      return this.data ? ((Array.isArray(this.data) ? this.data : [this.data]) as string[]) : []
-    },
-  },
-  created() {
+  setup(props, { slots }) {
+    const loaded = ref(false)
+    const fetching = ref(false)
+    const observer = ref<IntersectionObserver | null>(null)
+    const elementRef = ref<HTMLElement | null>(null)
     const page = usePage()
 
-    this.$watch(
-      () => this.keys.map((key) => get(page.props, key)),
-      () => {
-        const exists = this.keys.length > 0 && this.keys.every((key) => get(page.props, key) !== undefined)
-        this.loaded = exists
+    const keys = computed(() => {
+      return props.data ? ((Array.isArray(props.data) ? props.data : [props.data]) as string[]) : []
+    })
 
-        if (exists && !this.always) {
-          return
-        }
+    function getReloadParams(): Partial<ReloadOptions> {
+      const reloadParams: Partial<ReloadOptions> = { preserveErrors: true, ...props.params }
 
-        if (!this.observer || !exists) {
-          this.$nextTick(this.registerObserver)
-        }
-      },
-      { immediate: true },
-    )
-  },
-  methods: {
-    registerObserver() {
-      this.observer?.disconnect()
+      if (props.data) {
+        reloadParams.only = (Array.isArray(props.data) ? props.data : [props.data]) as string[]
+      }
 
-      this.observer = new IntersectionObserver(
+      return reloadParams
+    }
+
+    function registerObserver() {
+      observer.value?.disconnect()
+
+      observer.value = new IntersectionObserver(
         (entries) => {
           if (!entries[0].isIntersecting) {
             return
           }
 
-          if (this.fetching) {
+          if (fetching.value) {
             return
           }
 
-          if (!this.always && this.loaded) {
+          if (!props.always && loaded.value) {
             return
           }
 
-          this.fetching = true
+          fetching.value = true
 
-          const reloadParams = this.getReloadParams()
+          const reloadParams = getReloadParams()
 
           router.reload({
             ...reloadParams,
             onStart: (e) => {
-              this.fetching = true
+              fetching.value = true
               reloadParams.onStart?.(e)
             },
             onFinish: (e) => {
-              this.loaded = true
-              this.fetching = false
+              loaded.value = true
+              fetching.value = false
               reloadParams.onFinish?.(e)
 
-              if (!this.always) {
-                this.observer?.disconnect()
+              if (!props.always) {
+                observer.value?.disconnect()
               }
             },
           })
         },
         {
-          rootMargin: `${this.$props.buffer}px`,
+          rootMargin: `${props.buffer}px`,
         },
       )
 
-      this.observer.observe(this.$el.nextSibling)
-    },
-    getReloadParams(): Partial<ReloadOptions> {
-      const reloadParams: Partial<ReloadOptions> = { preserveErrors: true, ...this.$props.params }
+      if (elementRef.value) {
+        observer.value.observe(elementRef.value)
+      }
+    }
 
-      if (this.$props.data) {
-        reloadParams.only = (Array.isArray(this.$props.data) ? this.$props.data : [this.$props.data]) as string[]
+    watch(
+      () => keys.value.map((key) => get(page.props, key)),
+      () => {
+        const exists = keys.value.length > 0 && keys.value.every((key) => get(page.props, key) !== undefined)
+        loaded.value = exists
+
+        if (exists && !props.always) {
+          return
+        }
+
+        if (!observer.value || !exists) {
+          nextTick(registerObserver)
+        }
+      },
+      { immediate: true },
+    )
+
+    onUnmounted(() => {
+      observer.value?.disconnect()
+    })
+
+    return () => {
+      const els = []
+
+      if (props.always || !loaded.value) {
+        els.push(h(props.as, { ref: elementRef }))
       }
 
-      return reloadParams
-    },
-  },
-  render() {
-    const els = []
+      if (!loaded.value) {
+        els.push(slots.fallback ? slots.fallback({}) : null)
+      } else if (slots.default) {
+        els.push(slots.default({ fetching: fetching.value }))
+      }
 
-    if (this.$props.always || !this.loaded) {
-      els.push(h(this.$props.as))
+      return els
     }
-
-    if (!this.loaded) {
-      els.push(this.$slots.fallback ? this.$slots.fallback({}) : null)
-    } else if (this.$slots.default) {
-      els.push(this.$slots.default({ fetching: this.fetching }))
-    }
-
-    return els
   },
 })
