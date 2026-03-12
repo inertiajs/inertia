eaea82cbdfec1977fe0df3fd9083c626acf026db
Pascal Baljet <pascal@pascalbaljet.nl>
2026-03-12 11:32:46 +0100

    wip

--- packages/react/test-app/Pages/TypeScriptCreateInertiaApp.ts
+++ packages/react/test-app/Pages/TypeScriptCreateInertiaApp.ts
@@ -1,6 +1,5 @@
 // This file is used for checking the TypeScript implementation; there is no Playwright test depending on it.
 import { type ResolvedComponent, createInertiaApp } from '@inertiajs/react'
-import { createRoot } from 'react-dom/client'
 
 declare module '@inertiajs/core' {
   export interface InertiaConfig {
@@ -16,11 +15,9 @@ createInertiaApp({
     const pages = import.meta.glob<ResolvedComponent>('./Pages/**/*.tsx', { eager: true })
     return pages[`./Pages/${name}.tsx`]
   },
-  setup({ el, App, props }) {
+  setup({ props }) {
     console.log(props.initialPage.props.auth.user?.name)
     // @ts-expect-error - 'email' does not exist on user
     console.log(props.initialPage.props.auth.user?.email)
-
-    createRoot(el).render(<App {...props} />)
   },
 })
