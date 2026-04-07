af1c6ffe331542addded2d2d75ec4212999b73f8
Pascal Baljet <pascal@pascalbaljet.nl>
2026-04-07 12:33:30 +0200

    Svelte/Vue

---
+++ packages/vue3/test-app/Pages/LayoutProps/CallbackFunctionProp.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { Link } from '@inertiajs/vue3'
+
+defineOptions({
+  layout: () => ({ title: 'Function Prop Title', showSidebar: false, formatName: (name: string) => `Hello, ${name}` }),
+})
+</script>
+
+<template>
+  <div>
+    <h2>Callback Function Prop Page</h2>
+
+    <nav>
+      <Link href="/layout-props/basic">Go to Basic Page</Link>
+    </nav>
+  </div>
+</template>
