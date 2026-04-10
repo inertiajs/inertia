a4100695efb9242e8f4ea6a327647432d1bcb31d
Svyatoslav Kryukov <me@skryukov.dev>
2026-04-10 17:35:19 -0300

    Fix React useForm submitting stale data after synchronous setData

---
+++ packages/svelte/test-app/Pages/FormHelper/SetDataThenPost.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { useForm } from '@inertiajs/svelte'
+
+  const form = useForm({ code: 'initial' })
+
+  const handleSetAndPost = () => {
+    form.code = '123456'
+    form.post('/dump/post')
+  }
+
+  const handleDirty = () => {
+    form.code = 'dirty'
+  }
+
+  const handleResetAndPost = () => {
+    form.reset()
+    form.post('/dump/post')
+  }
+</script>
+
+<div>
+  <span id="current-code">{form.code}</span>
+  <button onclick={handleSetAndPost} class="set-and-post">Set and POST</button>
+  <button onclick={handleDirty} class="dirty">Dirty</button>
+  <button onclick={handleResetAndPost} class="reset-and-post">Reset and POST</button>
+</div>
---
+++ packages/vue3/test-app/Pages/FormHelper/SetDataThenPost.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { useForm } from '@inertiajs/vue3'
+
+const form = useForm({ code: 'initial' })
+
+const handleSetAndPost = () => {
+  form.code = '123456'
+  form.post('/dump/post')
+}
+
+const handleDirty = () => {
+  form.code = 'dirty'
+}
+
+const handleResetAndPost = () => {
+  form.reset()
+  form.post('/dump/post')
+}
+</script>
+
+<template>
+  <div>
+    <span id="current-code">{{ form.code }}</span>
+    <button @click="handleSetAndPost" class="set-and-post">Set and POST</button>
+    <button @click="handleDirty" class="dirty">Dirty</button>
+    <button @click="handleResetAndPost" class="reset-and-post">Reset and POST</button>
+  </div>
+</template>
