eba70fc72dc6e33a08e968fefc0c773ed3fe8a54
Pascal Baljet <pascal@pascalbaljet.nl>
2026-05-20 12:06:15 +0200

    Improve `router.poll` throttle semantics and stop+start handling (#3114)
    * Improve throttling and tests
    
    * Rest cleanup + tests
    
    * Update poll.ts
    
    * Update poll.spec.ts

--- packages/react/test-app/Pages/Poll/Overlap.tsx
+++ packages/react/test-app/Pages/Poll/Overlap.tsx
@@ -4,18 +4,24 @@ export default ({ mode, time }: { mode: string; time: number }) => {
   const params = new URLSearchParams(window.location.search)
   const interval = parseInt(params.get('interval') || '200')
 
-  const options: { mode?: 'overlap' | 'cancel' | 'rest' } = {}
+  const options: { mode?: 'overlap' | 'cancel' | 'rest'; keepAlive?: boolean } = {}
 
   if (mode === 'overlap' || mode === 'cancel' || mode === 'rest') {
     options.mode = mode
   }
 
-  usePoll(interval, {}, options)
+  if (params.get('keepAlive') === '1') {
+    options.keepAlive = true
+  }
+
+  const { start, stop } = usePoll(interval, {}, options)
 
   return (
     <div>
       <span id="mode">{mode}</span>
       <span id="time">{time}</span>
+      <button onClick={stop}>Stop</button>
+      <button onClick={start}>Start</button>
     </div>
   )
 }
--- packages/svelte/test-app/Pages/Poll/Overlap.svelte
+++ packages/svelte/test-app/Pages/Poll/Overlap.svelte
@@ -8,16 +8,22 @@
   const interval = parseInt(params.get('interval') || '200')
 
   const initialMode = mode
-  const options: { mode?: 'overlap' | 'cancel' | 'rest' } = {}
+  const options: { mode?: 'overlap' | 'cancel' | 'rest'; keepAlive?: boolean } = {}
 
   if (initialMode === 'overlap' || initialMode === 'cancel' || initialMode === 'rest') {
     options.mode = initialMode
   }
 
-  usePoll(interval, {}, options)
+  if (params.get('keepAlive') === '1') {
+    options.keepAlive = true
+  }
+
+  const { start, stop } = usePoll(interval, {}, options)
 </script>
 
 <div>
   <span id="mode">{mode}</span>
   <span id="time">{time}</span>
+  <button on:click={stop}>Stop</button>
+  <button on:click={start}>Start</button>
 </div>
