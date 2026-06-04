6049ed2a24b6679b313e7a0c4dedef2e45ba2b25
pascalbaljet <8403149+pascalbaljet@users.noreply.github.com>
2026-06-04 09:22:14 +0000

    Fix code style

--- playgrounds/vue3/resources/js/Components/Layout.vue
+++ playgrounds/vue3/resources/js/Components/Layout.vue
@@ -10,7 +10,7 @@ const appName = computed(() => page.props.appName)
 </script>
 
 <template>
-  <nav class="flex items-center space-x-6 bg-slate-800 px-10 py-6 text-white overflow-scroll">
+  <nav class="flex items-center space-x-6 overflow-scroll bg-slate-800 px-10 py-6 text-white">
     <div class="rounded-lg bg-slate-700 px-4 py-1">{{ appName }}</div>
     <Link href="/" class="hover:underline" prefetch>Home</Link>
     <Link href="/users" class="hover:underline" prefetch :cache-for="['2s', '1m']">Users</Link>
