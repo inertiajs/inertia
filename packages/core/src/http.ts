1bd623f7268283f2c849b61254aa89ba81f9074a
Pascal Baljet <pascal@pascalbaljet.nl>
2026-06-10 13:15:01 +0200

    [2.x] Add `http` export (#3149)
    * Add `http` export
    
    * Fix code style

---
+++ packages/core/src/http.ts
@@ -0,0 +1,6 @@
+/**
+ * Inertia v3 exposes a configurable HTTP client as `http`, but v2 has none.
+ * Exported as `undefined` here so libraries supporting both majors can
+ * feature-detect it with a truthy check: `if (http) http.getClient()`.
+ */
+export const http = undefined
