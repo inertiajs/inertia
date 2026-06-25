17bd32ca6dbc008a014265891cf9902d7dd385e4
Pascal Baljet <pascal@pascalbaljet.nl>
2026-06-25 10:34:19 +0200

    Formatting

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -778,7 +778,15 @@ export type UseHttpSubmitArguments<TResponse = unknown, TForm = unknown> =
 
 export type FormComponentOptions = Pick<
   VisitOptions,
-  'preserveScroll' | 'preserveState' | 'preserveUrl' | 'replace' | 'only' | 'except' | 'reset' | 'viewTransition' | 'async'
+  | 'preserveScroll'
+  | 'preserveState'
+  | 'preserveUrl'
+  | 'replace'
+  | 'only'
+  | 'except'
+  | 'reset'
+  | 'viewTransition'
+  | 'async'
 >
 
 export type FormComponentOptimisticCallback<
