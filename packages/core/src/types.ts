01538be78849f4c714bb1abea00dc1bf6226051d
pascalbaljet <8403149+pascalbaljet@users.noreply.github.com>
2026-06-25 07:36:58 +0000

    Fix code style

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -649,7 +649,15 @@ export type UseFormSubmitArguments =
 
 export type FormComponentOptions = Pick<
   VisitOptions,
-  'preserveScroll' | 'preserveState' | 'preserveUrl' | 'replace' | 'only' | 'except' | 'reset' | 'viewTransition' | 'async'
+  | 'preserveScroll'
+  | 'preserveState'
+  | 'preserveUrl'
+  | 'replace'
+  | 'only'
+  | 'except'
+  | 'reset'
+  | 'viewTransition'
+  | 'async'
 >
 
 export type FormComponentProps = Partial<
